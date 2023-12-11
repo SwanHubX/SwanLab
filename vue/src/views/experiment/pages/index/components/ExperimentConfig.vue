16f59cab3d2d8ecf4f02b3a91aae443f458bca90
Zirui Cai <1575079435@qq.com>
2023-12-11 18:01:55 +0800

    feat: extend block

---
+++ vue/src/views/experiment/pages/index/components/ExperimentConfig.vue
@@ -0,0 +1,19 @@
+<template>
+  <ExtendBlock class="py-6" icon="experiment" title="配置信息">
+    <div class="pl-6 w-full">
+      <p class="font-semibold py-4">配置</p>
+      <div class="w-full border">111</div>
+    </div>
+  </ExtendBlock>
+</template>
+
+<script setup>
+/**
+ * @description: 实验的配置信息
+ * @file: ExperimentConfig.vue
+ * @since: 2023-12-11 17:07:31
+ **/
+import ExtendBlock from '@swanlab-vue/views/experiment/components/ExtendBlock.vue'
+</script>
+
+<style lang="scss" scoped></style>
