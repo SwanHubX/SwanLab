c94ab2fef8a69345f2b6edbf3062f12c6a354b7c
ZeYi Lin <944270057@qq.com>
2023-12-12 23:15:49 +0800

    generate_color函数增加参数number
    逻辑改为输入1个数字，输出颜色列表中的颜色

--- swanlab/utils/color.py
+++ swanlab/utils/color.py
@@ -47,8 +47,8 @@ def hex_to_rgb(hex_color: str):
     return (r, g, b)
 
 
-def generate_color() -> str:
-    """生成十六进制颜色字符串
+def generate_color(number: int = 0) -> str:
+    """输入数字，在设定好顺序的颜色列表中返回十六进制颜色字符串
 
     Returns
     -------
@@ -58,43 +58,43 @@ def generate_color() -> str:
     """
 
     # 生成 RGB 随机变化值
-    r_random = random.randint(0, 10)
-    g_random = random.randint(0, 10)
-    b_random = random.randint(0, 10)
+    # r_random = random.randint(0, 10)
+    # g_random = random.randint(0, 10)
+    # b_random = random.randint(0, 10)
 
     # 生成随机数, 用于在颜色列表中选择一个随机颜色
-    random_number = random.randint(0, 15)
+    # random_number = random.randint(0, 15)
 
     color_list = [
-        "#528d59",
-        "#9cbe5d",
-        "#dfb142",
-        "#d0703c",
-        "#e3b292",
-        "#c24d46",
-        "#892d58",
-        "#d47694",
-        "#8cc5b7",
-        "#40877c",
-        "#6ebad3",
-        "#587ad2",
-        "#6d4ba4",
-        "#b15fbb",
-        "#905f4a",
-        "#989fa3",
+        "#528d59",  # 绿色
+        "#587ad2",  # 蓝色
+        "#c24d46",  # 红色
+        "#9cbe5d",  # 青绿色
+        "#6ebad3",  # 天蓝色
+        "#dfb142",  # 橙色
+        "#6d4ba4",  # 紫色
+        "#8cc5b7",  # 淡青绿色
+        "#892d58",  # 紫红色
+        "#40877c",  # 深青绿色
+        "#d0703c",  # 深橙色
+        "#d47694",  # 粉红色
+        "#e3b292",  # 淡橙色
+        "#b15fbb",  # 浅紫红色
+        "#905f4a",  # 棕色
+        "#989fa3",  # 灰色
     ]
 
     # 将随机选择的十六进制字符串转为RGB
-    r, g, b = hex_to_rgb(color_list[random_number])
+    # r, g, b = hex_to_rgb(color_list[random_number])
 
-    # 在RGB通道增加随机波动
-    r = min(r + r_random, 255)
-    g = min(g + g_random, 255)
-    b = min(b + b_random, 255)
+    # # 在RGB通道增加随机波动
+    # r = min(r + r_random, 255)
+    # g = min(g + g_random, 255)
+    # b = min(b + b_random, 255)
 
-    # 将RGB转为十六进制字符串，然后返回
-    return rgb_to_hex(rgb_color=(r, g, b))
+    # 输入
+    return color_list[number]
 
 
 if __name__ == "__main__":
-    print(generate_color())
+    print(generate_color(2))
