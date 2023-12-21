324f00afe374dcfd172d472f4e4f4f635235a4f8
KAAANG <cunykang@gmail.com>
2023-12-21 18:12:17 +0800

    add font

--- vue/src/views/experiment/pages/log/LogPage.vue
+++ vue/src/views/experiment/pages/log/LogPage.vue
@@ -1,63 +1,12 @@
 <template>
   <div class="w-full h-full px-7 py-6">
-    <section class="bg-dimmer w-full h-full overflow-auto rounded p-4">
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
-      <div>dsadsadasdsadsa</div>
+    <section class="log-area">
+      <div class="log-line" v-for="i in 1000" :key="i">
+        <!-- 行数 -->
+        <span>{{ i }}</span>
+        <!-- 日志内容 -->
+        <span> dsadsadasdsadsa </span>
+      </div>
     </section>
   </div>
 </template>
@@ -70,4 +19,23 @@
  **/
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.log-area {
+  @apply bg-dimmer w-full h-full overflow-auto rounded p-4;
+  font-size: 13px;
+  line-height: 16px;
+  font-family: 'JetBrains Mono', monospace;
+  letter-spacing: 0.1px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  .log-line {
+    @apply flex gap-2;
+    span {
+      @apply block;
+    }
+    span:first-child {
+      @apply w-8 text-right text-gray-400;
+    }
+  }
+}
+</style>
