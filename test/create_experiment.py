99efc7e8883d80fd1a3e2a288e10e37d01ca1ed7
KAAANG <cunykang@gmail.com>
2023-12-29 15:43:19 +0800

    rebase

--- test/create_experiment.py
+++ test/create_experiment.py
@@ -11,23 +11,8 @@ import random
 import swanlab as sw
 import time
 
-
-class Enlarge1000Type(sw.BaseType):
-    def get_data(self):
-        return int(self.value * 1000)
-
-    def get_chart_type(self) -> str:
-        return self.chart.line
-
-    def get_namespace(self) -> str:
-        return "custom"
-
-    def get_config(self) -> dict:
-        return {"color": "#000000"}
-
-
 # 迭代次数
-epochs = 5
+epochs = 100
 # 学习率
 lr = 0.01
 # 随机偏移量
@@ -51,10 +36,10 @@ for epoch in range(2, epochs):
     acc = 1 - 2**-epoch - random.random() / epoch - offset
     loss = 2**-epoch + random.random() / epoch + offset
     print(f"epoch={epoch}, accuracy={acc}, loss={loss}")
-    sw.log({"test/loss": loss, "accuracy": acc})
+    sw.log({"loss": loss, "accuracy": acc})
     sw.log({"loss2": loss, "accuracy2": acc}, step=epochs - epoch)
-    sw.log({"train/loss3": loss, "accuracy3": acc}, step=epoch * 2)
-    sw.log({"int": Enlarge1000Type(loss)}, step=epoch * 3)
+    sw.log({"loss3": loss, "accuracy3": acc}, step=epoch * 2)
+
     time.sleep(0.5)
     # if epoch % 40 == 0:
     #     epoch / 0
