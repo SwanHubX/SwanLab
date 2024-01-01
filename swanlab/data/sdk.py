4648328a8bfbfee6d8c3bf5e8efce4834fa4d3c2
KAAANG <cunykang@gmail.com>
2024-01-01 18:01:58 +0800

    add sdk

---
+++ swanlab/data/sdk.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""
+@DATE: 2024-01-01 18:00:04
+@File: swanlab/data/sdk.py
+@IDE: vscode
+@Description:
+    在此处封装swanlab在日志记录模式下的各种借口
+"""
+
+
+def init(experiment_name: str = None, description: str = "", config: dict = {}, *args, **kwargs):
+    pass
+
+
+def log(data: dict, step: int = None):
+    pass
