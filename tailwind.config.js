f7052be80980fd00dd291c10d842f997eb7e3312
KAAANG <cunykang@gmail.com>
2024-01-03 21:02:45 +0800

    cover color

--- tailwind.config.js
+++ tailwind.config.js
@@ -47,31 +47,33 @@ export default {
         dimmest: 'var(--foreground-dimmest, #b5b5b5)',
         // 复杂文字高亮
         'white-default': 'var(--accent-white-default, #fff)'
-      },
-      // 基础颜色
-      colors: {
-        /* 成功颜色 */
-        'positive-highest': 'var(--positive-highest, #004d0d)',
-        'positive-higher': 'var(--positive-higher, #036e15)',
-        'positive-default': 'var(--positive-default, #00a11b)',
-        'positive-dimmer': 'var(--positive-dimmer, #3cc954)',
-        'positive-dimmest': 'var(--positive-dimmest, #97e896)',
-        /* 标准颜色 */
-        'primary-highest': 'var(--primary-highest, #004182)',
-        'primary-higher': 'var(--primary-higher, #005cb8)',
-        'primary-default': 'var(--primary-default, #0f87ff)',
-        'primary-dimmer': 'var(--primary-dimmer, #74b9ff)',
-        'primary-dimmest': 'var(--primary-dimmest, #b2d9ff)',
-        /* 警告颜色 */
-        'warning-dimmer': 'var(--warning-dimmer, #ff9933)',
-        'warning-dimmest': 'var(--warning-dimmest, #ffad5a)',
-        /* 危险颜色 */
-        'negative-highest': 'var(--negative-highest, #8a0000)',
-        'negative-higher': 'var(--negative-higher, #c20a0a)',
-        'negative-default': 'var(--negative-default, #fa4b4b)',
-        'negative-dimmest': 'var(--negative-dimmest, #ff9494)',
-        'negative-dimmer': 'var(--negative-dimmer, #ffc7c7)'
       }
+    },
+    // 基础颜色
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      /* 成功颜色 */
+      'positive-highest': 'var(--positive-highest, #004d0d)',
+      'positive-higher': 'var(--positive-higher, #036e15)',
+      'positive-default': 'var(--positive-default, #00a11b)',
+      'positive-dimmer': 'var(--positive-dimmer, #3cc954)',
+      'positive-dimmest': 'var(--positive-dimmest, #97e896)',
+      /* 标准颜色 */
+      'primary-highest': 'var(--primary-highest, #004182)',
+      'primary-higher': 'var(--primary-higher, #005cb8)',
+      'primary-default': 'var(--primary-default, #0f87ff)',
+      'primary-dimmer': 'var(--primary-dimmer, #74b9ff)',
+      'primary-dimmest': 'var(--primary-dimmest, #b2d9ff)',
+      /* 警告颜色 */
+      'warning-dimmer': 'var(--warning-dimmer, #ff9933)',
+      'warning-dimmest': 'var(--warning-dimmest, #ffad5a)',
+      /* 危险颜色 */
+      'negative-highest': 'var(--negative-highest, #8a0000)',
+      'negative-higher': 'var(--negative-higher, #c20a0a)',
+      'negative-default': 'var(--negative-default, #fa4b4b)',
+      'negative-dimmest': 'var(--negative-dimmest, #ff9494)',
+      'negative-dimmer': 'var(--negative-dimmer, #ffc7c7)'
     }
   },
   plugins: []
