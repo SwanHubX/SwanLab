1c4b9741937b6b82bc2cf47107f769404e6ba6f8
Zirui Cai <1575079435@qq.com>
2024-01-05 15:18:08 +0800

    ui: some modifications

--- vue/src/views/experiment/pages/index/components/ExperimentHeader.vue
+++ vue/src/views/experiment/pages/index/components/ExperimentHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full px-6 pt-6 text-dimmer relative">
+  <div class="w-full px-6 pt-6 text-default relative">
     <!-- 删除项目 -->
     <SLDelete
       class="absolute top-5 right-4"
@@ -23,15 +23,16 @@
       <div class="min-w-[400px] max-w-[50%] pr-4">
         <!-- 实验状态 -->
         <div class="flex items-center pb-4">
-          <div class="min-w-[150px]">{{ $t(`experiment.index.header.experiment_infos.status`) }}</div>
+          <div class="title">{{ $t(`experiment.index.header.experiment_infos.status`) }}</div>
           <SLStatusLabel :name="experiment.name" :id="experiment.id" :status="experiment.status" />
           <!-- 停止按钮 -->
           <!-- <StopButton v-if="experimentStore.isRunning" /> -->
         </div>
         <div v-for="item in experiment_infos" :key="item.title">
-          <div class="flex pb-4" v-if="item.value">
-            <div class="min-w-[150px]">{{ $t(`experiment.index.header.experiment_infos.${item.title}`) }}</div>
+          <div class="flex pb-4" v-if="item.value || item.title === 'git'">
+            <div class="title">{{ $t(`experiment.index.header.experiment_infos.${item.title}`) }}</div>
             <div v-if="!item.isLink" :title="item.value">{{ item.value }}</div>
+            <div v-else-if="item.title === 'git' && !item.value" class="text-dimmest">None</div>
             <a :href="item.value" target="_blank" class="hover:underline break-all" v-else>{{ item.value }}</a>
           </div>
         </div>
@@ -39,7 +40,7 @@
       <!-- 系统相关 -->
       <div class="w-1/2 min-w-[400px]" v-if="experiment.system">
         <div v-for="item in experiment_device" :key="item.title" class="flex pb-4">
-          <div class="min-w-[150px]">{{ $t(`experiment.index.header.experiment_device.${item.title}`) }}</div>
+          <div class="title">{{ $t(`experiment.index.header.experiment_device.${item.title}`) }}</div>
           <div :title="item.value">{{ item.value || 'Unkown' }}</div>
         </div>
       </div>
@@ -211,5 +212,9 @@ const deleteExperiment = () => {
 <style lang="scss" scoped>
 .icon {
   @apply w-5 h-5 text-dimmest cursor-pointer hover:text-dimmer;
+}
+
+.title {
+  @apply min-w-[150px] text-dimmest;
 }
 </style>
