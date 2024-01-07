ddc324e4d3fd202dbacc31a7f5c704d4be4708aa
Zirui Cai <1575079435@qq.com>
2024-01-07 15:57:46 +0800

    refactor: stop experiment

--- vue/src/views/experiment/pages/index/components/StopButton.vue
+++ vue/src/views/experiment/pages/index/components/StopButton.vue
@@ -1,15 +1,6 @@
 <template>
-  <button
-    class="p-1.5 rounded-full border-2 transition-all duration-200 ml-2"
-    :class="showColor ? 'border-negative-default' : 'cursor-not-allowed'"
-    @mouseover="() => (hover = true)"
-    @mouseout="() => (hover = false)"
-    @click="stop"
-  >
-    <div
-      class="w-2.5 h-2.5 transition-all duration-200"
-      :class="showColor ? 'bg-negative-default' : 'bg-overlay opacity-50'"
-    ></div>
+  <button class="underline text-dimmest ml-2" :class="showColor ? '' : 'cursor-not-allowed'" @click="stop">
+    is over?
   </button>
 </template>
 
@@ -20,11 +11,11 @@
  * @since: 2023-12-30 20:30:30
  **/
 import { useExperimentStroe, useProjectStore } from '@swanlab-vue/store'
-import { ref } from 'vue'
 import { computed } from 'vue'
 import { confirm } from '@swanlab-vue/components/comfirm'
 import http from '@swanlab-vue/api/http'
 import { t } from '@swanlab-vue/i18n'
+import { message } from '@swanlab-vue/components/message'
 
 // ---------------------------------- 弹窗相关 ----------------------------------
 
@@ -32,10 +23,9 @@ const experiment = useExperimentStroe()
 const id = experiment.id
 const status = experiment.status
 
-const hover = ref(false) // 是否hover
 // 展示hover时可点击样式
 const showColor = computed(() => {
-  return status === 0 && hover.value
+  return status === 0
 })
 
 // ---------------------------------- 确认删除 ----------------------------------
@@ -53,6 +43,7 @@ const stop_experiment = async () => {
   if (!data) return
   experiment.setUpateTIme(data.update_time)
   useProjectStore().setExperimentStatus(id, -1)
+  message.success('State changed')
 }
 </script>
 
