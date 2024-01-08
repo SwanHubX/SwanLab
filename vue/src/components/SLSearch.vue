da8fa92303aab75dd32d663556e7e90e0ced7d12
Zirui Cai <74649535+Feudalman@users.noreply.github.com>
2024-01-08 19:41:17 +0800

    Feature/log operation (#173)
    * feat：pull down
    
    * feat: search in log
    
    * fix: lower and upper case
    
    * fix: hight

---
+++ vue/src/components/SLSearch.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="w-full border flex gap-2 justify-between items-center py-1 px-2 rounded-lg">
+    <SLIcon class="w-4 h-4" icon="search"></SLIcon>
+    <input
+      type="text"
+      v-model="value"
+      class="w-full outline-none"
+      @input="input"
+      @keydown.enter="$emit('search', value)"
+    />
+  </div>
+</template>
+
+<script setup>
+/**
+ * @description: 搜索
+ * @file: SLSearch.vue
+ * @since: 2024-01-08 16:36:14
+ **/
+
+import { ref } from 'vue'
+import SLIcon from './SLIcon.vue'
+import { debounce } from '@swanlab-vue/utils/common'
+
+const emits = defineEmits(['input', 'search'])
+
+const props = defineProps({
+  dealy: {
+    type: String,
+    default: '500'
+  }
+})
+
+const value = ref('')
+
+const input = debounce(() => {
+  emits('input', value.value)
+}, props.dealy)
+</script>
+
+<style lang="scss" scoped></style>
--- vue/src/views/experiment/pages/log/LogPage.vue
+++ vue/src/views/experiment/pages/log/LogPage.vue
@@ -1,13 +1,29 @@
 <template>
-  <div class="w-full h-full px-7 py-6">
+  <div class="w-full h-full px-7 py-6 relative overflow-hidden">
+    <SLSearch class="max-w-[400px] mb-5" @input="search" dealy="300" />
     <section class="log-container">
       <div class="log-area" ref="logAreaRef" v-if="logs">
-        <div class="log-line" v-for="line in logs" :key="line">
+        <!-- 运行日志 -->
+        <div class="log-line" v-for="line in filteredLogs" :key="line">
           <!-- 行号 -->
-          <span>{{ line.substring(0, line.indexOf(' ')) }}</span>
+          <span class="w-8 text-right flex-shrink-0 text-dimmest select-none">{{ line.index }}</span>
           <!-- 日志内容 -->
-          <span>{{ line.substring(line.indexOf(' ')) }}</span>
+          <!-- 如果没有搜索内容/不含搜索内容 -->
+          <span v-show="!searchValue || !line.lower.includes(searchValue)">{{ line.value }} </span>
+          <!-- 如果有搜索内容且含有搜索内容 -->
+          <p class="flex" v-show="searchValue && line.lower.includes(searchValue)">
+            <span>{{ line.value.substring(0, line.lower.indexOf(searchValue)) }}</span>
+            <!-- 高亮展示 -->
+            <span class="bg-[#F6FFA6]">{{
+              line.value.substring(
+                line.lower.indexOf(searchValue),
+                line.lower.indexOf(searchValue) + searchValue.length
+              )
+            }}</span>
+            <span>{{ line.value.substring(line.lower.indexOf(searchValue) + searchValue.length) }}</span>
+          </p>
         </div>
+        <!-- 错误日志 -->
         <div class="log-line text-negative-default" v-for="(line, index) in errorLogs" :key="line">
           <!-- 行数 -->
           <span>{{ logs.length + index }}</span>
@@ -34,15 +50,32 @@ import http from '@swanlab-vue/api/http'
 import { useExperimentStroe } from '@swanlab-vue/store'
 import { addTaskToBrowserMainThread } from '@swanlab-vue/utils/browser'
 import SLLoding from '@swanlab-vue/components/SLLoading.vue'
+import SLSearch from '@swanlab-vue/components/SLSearch.vue'
+import { computed } from 'vue'
+
 const logAreaRef = ref()
+
 // ---------------------------------- 系统相关 ----------------------------------
 
 const experimentStore = useExperimentStroe()
 const id = experimentStore.id
 
 // ---------------------------------- 获取日志和日志相关数据 ----------------------------------
+
 // 所有日志
 const logs = ref()
+
+// 分开行号和内容之后的日志
+const filteredLogs = computed(() => {
+  return logs.value.map((line) => {
+    return {
+      index: line.substring(0, line.indexOf(' ')),
+      value: line.substring(line.indexOf(' ')),
+      lower: line.substring(line.indexOf(' ')).toLowerCase()
+    }
+  })
+})
+
 // 错误日志
 const errorLogs = ref([])
 ;(async function () {
@@ -56,11 +89,19 @@ const errorLogs = ref([])
     logAreaRef.value.scrollTop = logAreaRef.value.scrollHeight
   })
 })()
+
+// ---------------------------------- 搜索 ----------------------------------
+
+const searchValue = ref('')
+
+const search = (value) => {
+  searchValue.value = value.toLowerCase()
+}
 </script>
 
 <style lang="scss" scoped>
 .log-container {
-  @apply bg-higher w-full h-full rounded p-4;
+  @apply bg-higher w-full h-[calc(100vh-180px)] rounded p-4;
   font-size: 13px;
   line-height: 16px;
   font-family: 'JetBrains Mono', monospace;
@@ -75,12 +116,9 @@ const errorLogs = ref([])
   }
 
   .log-line {
-    @apply flex gap-2;
+    @apply flex gap-2 whitespace-pre-wrap;
     span {
       @apply block;
-    }
-    span:first-child {
-      @apply w-8 text-right flex-shrink-0 text-dimmest select-none;
     }
   }
 }
