a0a2ff7d6466e2c4d0c9f14e7210adb8a09c50f8
Zirui Cai <1575079435@qq.com>
2024-01-08 17:40:30 +0800

    feat: search in log

--- vue/src/views/experiment/pages/log/LogPage.vue
+++ vue/src/views/experiment/pages/log/LogPage.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="w-full h-full px-7 py-6 relative overflow-hidden">
+    <SLSearch class="max-w-[400px]" @input="search" dealy="100" />
     <section class="log-container">
       <div class="log-area" ref="logAreaRef" v-if="logs">
-        <div class="log-line" v-for="line in logs" :key="line">
+        <!-- 运行日志 -->
+        <div class="log-line" v-for="line in filteredLogs" :key="line">
           <!-- 行号 -->
-          <span>{{ line.substring(0, line.indexOf(' ')) }}</span>
+          <span class="w-8 text-right flex-shrink-0 text-dimmest select-none">{{ line.index }}</span>
           <!-- 日志内容 -->
-          <span>{{ line.substring(line.indexOf(' ')) }}</span>
+          <!-- 如果没有搜索内容/不含搜索内容 -->
+          <span v-show="!searchValue || !line.value.includes(searchValue)">{{ line.value }} </span>
+          <!-- 如果有搜索内容且含有搜索内容 -->
+          <p class="flex" v-show="searchValue && line.value.includes(searchValue)">
+            <span>{{ line.value.substring(0, line.value.indexOf(searchValue)) }}</span>
+            <!-- 高亮展示 -->
+            <span class="bg-negative-default text-white-default">{{ searchValue }}</span>
+            <span>{{ line.value.substring(line.value.indexOf(searchValue) + searchValue.length) }}</span>
+          </p>
         </div>
+        <!-- 错误日志 -->
         <div class="log-line text-negative-default" v-for="(line, index) in errorLogs" :key="line">
           <!-- 行数 -->
           <span>{{ logs.length + index }}</span>
@@ -39,17 +50,31 @@ import { useExperimentStroe } from '@swanlab-vue/store'
 import { addTaskToBrowserMainThread } from '@swanlab-vue/utils/browser'
 import SLLoding from '@swanlab-vue/components/SLLoading.vue'
 import SLOperation from '@swanlab-vue/components/SLOperation.vue'
-import SLButton from '@swanlab-vue/components/SLButton.vue'
+import SLSearch from '@swanlab-vue/components/SLSearch.vue'
+import { computed } from 'vue'
 
 const logAreaRef = ref()
+
 // ---------------------------------- 系统相关 ----------------------------------
 
 const experimentStore = useExperimentStroe()
 const id = experimentStore.id
 
 // ---------------------------------- 获取日志和日志相关数据 ----------------------------------
+
 // 所有日志
 const logs = ref()
+
+// 分开行号和内容之后的日志
+const filteredLogs = computed(() => {
+  return logs.value.map((line) => {
+    return {
+      index: line.substring(0, line.indexOf(' ')),
+      value: line.substring(line.indexOf(' '))
+    }
+  })
+})
+
 // 错误日志
 const errorLogs = ref([])
 ;(async function () {
@@ -63,6 +88,14 @@ const errorLogs = ref([])
     logAreaRef.value.scrollTop = logAreaRef.value.scrollHeight
   })
 })()
+
+// ---------------------------------- 搜索 ----------------------------------
+
+const searchValue = ref('')
+
+const search = (value) => {
+  searchValue.value = value
+}
 </script>
 
 <style lang="scss" scoped>
@@ -82,12 +115,9 @@ const errorLogs = ref([])
   }
 
   .log-line {
-    @apply flex gap-2;
+    @apply flex gap-2 whitespace-pre-wrap;
     span {
       @apply block;
-    }
-    span:first-child {
-      @apply w-8 text-right flex-shrink-0 text-dimmest select-none;
     }
   }
 }
