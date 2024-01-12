9b35f37f05891ebb878cee0e65f0cc2078fd6d1f
Zirui Cai <1575079435@qq.com>
2024-01-12 15:00:37 +0800

    feat: number

---
+++ swanlab/utils/number.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ---------------------------------- 科学计数法 ----------------------------------
+
+
+class ScientificNotation:
+    # 转化阈值 —— 最小值
+    __min_threshold = 1e-21
+
+    # 转化阈值 —— 最大值
+    __max_threshold = 1e10
+
+    def __init__(self, min_threshold: float = 1e-21, max_threshold: float = 1e10):
+        self.__min_threshold = min_threshold
+        self.__max_threshold = max_threshold
+
+    def format(self, value, digits=6) -> str:
+        """将超出阈值的数字转成科学计数法
+        为了便于存储，在返回时使用字符串格式
+
+        Parameters
+        ----------
+        value : float
+            需要判断和转化的格式
+        digits : int
+            科学计数法时的保留位数
+
+        Returns
+        -------
+        str
+            转化后的数字
+        """
+        if self.__min_threshold <= value <= self.__max_threshold:
+            return str(value)
+        else:
+            return format(value, f".{digits}e")
