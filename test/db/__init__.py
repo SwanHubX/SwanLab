4119abf02cd478ebd4ec3e23166ad93922e6d773
Zirui Cai <74649535+Feudalman@users.noreply.github.com>
2024-01-20 14:48:25 +0800

    feat: atomic

--- test/db/__init__.py
+++ test/db/__init__.py
@@ -8,6 +8,7 @@ r"""
     每次一个个添加数据太麻烦了，写一个统筹的东西
 """
 
+from swanlab.db.settings import swandb
 from swanlab.db import (
     Project,
     Experiment,
@@ -19,25 +20,26 @@ from swanlab.db import (
 )
 
 
-Project.init()
+with swandb.atomic():
+    Project.init()
 
-Experiment.create("experiment1", "run1")
-Experiment.create("experiment2", "run2")
-Experiment.create("experiment3", "run3")
+    Experiment.create("experiment1", "run1")
+    Experiment.create("experiment2", "run2")
+    Experiment.create("experiment3", "run3")
 
-Tag.create(experiment_id=1, name="tag1", type="int")
-Tag.create(experiment_id=2, name="tag2", type="int")
-Tag.create(experiment_id=3, name="tag3", type="int")
-Tag.create(experiment_id=1, name="tag11", type="int")
-Tag.create(experiment_id=2, name="tag1", type="int")
+    Tag.create(experiment_id=1, name="tag1", type="int")
+    Tag.create(experiment_id=2, name="tag2", type="int")
+    Tag.create(experiment_id=3, name="tag3", type="int")
+    Tag.create(experiment_id=1, name="tag11", type="int")
+    Tag.create(experiment_id=2, name="tag1", type="int")
 
-Chart.create(name="chart1", description="123", project_id=1)
-Chart.create(name="chart2", description="123", project_id=1)
-Chart.create(name="chart11", description="123", experiment_id=1)
-Chart.create(name="chart22", description="123", experiment_id=1)
+    Chart.create(name="chart1", description="123", project_id=1)
+    Chart.create(name="chart2", description="123", project_id=1)
+    Chart.create(name="chart11", description="123", experiment_id=1)
+    Chart.create(name="chart22", description="123", experiment_id=1)
 
-Namespace.create(name="name1", experiment_id=1)
-Namespace.create(name="name2", experiment_id=1)
-Namespace.create(name="name3", experiment_id=1)
-Namespace.create(name="name11", project_id=1)
-Namespace.create(name="name22", project_id=1)
+    Namespace.create(name="name1", experiment_id=1)
+    Namespace.create(name="name2", experiment_id=1)
+    Namespace.create(name="name3", experiment_id=1)
+    Namespace.create(name="name11", project_id=1)
+    Namespace.create(name="name22", project_id=1)
