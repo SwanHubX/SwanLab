31487a2b4a93f3a61d9a618a527bb812685dbada
Zirui Cai <74649535+Feudalman@users.noreply.github.com>
2024-01-22 14:38:07 +0800

    feat: hover copy

--- vue/src/views/experiment/pages/environment/components/EnvItem.vue
+++ vue/src/views/experiment/pages/environment/components/EnvItem.vue
@@ -2,9 +2,20 @@
   <div class="w-full flex" v-if="envValue">
     <!-- key -->
     <span class="text-dimmer w-44 shrink-0">{{ $t(`experiment.env.keys.${envKey}`) }}</span>
-    <span class="break-all" :class="highLight ? 'text-dimmer bg-higher px-2' : ''">
+    <span
+      class="break-all relative"
+      :class="[highLight ? 'high-light' : '', copy ? 'copy' : '']"
+      @mouseenter="hover = true"
+      @mouseleave="hover = false"
+    >
       <span v-if="!link">{{ envValue }}</span>
       <a :href="envValue" target="_blank" class="hover:underline underline-offset-2" v-else>{{ envValue }}</a>
+      <SLCopy
+        :text="envValue"
+        class="absolute top-1 right-1 transition-all opacity-0"
+        :class="hover ? 'copy-button' : ''"
+        v-show="hover"
+      />
     </span>
   </div>
 </template>
@@ -16,9 +27,11 @@
  * @since: 2024-01-09 19:44:16
  **/
 
+import { ref } from 'vue'
+
 defineProps({
   envKey: {
-    type: String,
+    type: [String, Number],
     default: ''
   },
   envValue: {
@@ -32,8 +45,29 @@ defineProps({
   link: {
     type: Boolean,
     default: false
+  },
+  copy: {
+    type: Boolean,
+    default: true
   }
 })
+
+const hover = ref(false)
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.high-light {
+  @apply text-dimmer bg-higher px-2;
+}
+
+.copy {
+  @apply transition-all overflow-hidden;
+  &:hover {
+    @apply bg-higher pr-8;
+  }
+}
+
+.copy-button {
+  @apply opacity-100;
+}
+</style>
