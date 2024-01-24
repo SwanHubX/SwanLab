60ad458f2f21ca148579bba796800b2b47746da3
KAAANG <cunykang@gmail.com>
2024-01-24 21:02:14 +0800

    fix nan

--- test/create_experiment.py
+++ test/create_experiment.py
@@ -1,10 +1,11 @@
 import swanlab
 import time
+import random
 
 swanlab.init(log_level="debug")
 
 for i in range(100):
-    swanlab.log({"loss": i})
+    swanlab.log({"loss": random.choice([0.1, 0.2, 0.3, 0.4, "nan"]), "acc": i})
     time.sleep(0.1)
 
 for i in range(300):
