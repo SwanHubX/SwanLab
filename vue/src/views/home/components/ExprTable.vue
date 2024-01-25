293a700f71f9280e17bd0d8e910d625818b86d6d
Zirui Cai <74649535+Feudalman@users.noreply.github.com>
2024-01-25 13:57:34 +0800

    fix: overflow

--- vue/src/views/home/components/ExprTable.vue
+++ vue/src/views/home/components/ExprTable.vue
@@ -63,7 +63,7 @@
             <slot :name="item.slot" v-bind:row="dataColumn" v-bind:index="dataIndex"></slot>
           </div>
           <!-- 文本格式 -->
-          <div v-else>{{ dataColumn[item.key] || '-' }}</div>
+          <div class="w-full overflow-hidden" v-else>{{ dataColumn[item.key] || '-' }}</div>
         </div>
       </div>
       <!-- 没有数据时，空占位 -->
@@ -316,7 +316,7 @@ const handleMouseOver = (index) => {
 }
 
 .cell {
-  @apply text-left whitespace-nowrap shrink-0 overflow-hidden;
+  @apply text-left whitespace-nowrap shrink-0;
   @apply box-border;
   height: 54px;
 
