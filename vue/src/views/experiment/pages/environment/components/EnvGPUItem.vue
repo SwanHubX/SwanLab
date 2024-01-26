dc24d00bb23e52dfce5ad2237e73e2c872b5c60a
Zachary_Yang <115958756+little1d@users.noreply.github.com>
2024-01-26 10:51:11 +0800

    feat(EnvGpuItem.vue)Rendering gpu data using pinia

--- vue/src/views/experiment/pages/environment/components/EnvGPUItem.vue
+++ vue/src/views/experiment/pages/environment/components/EnvGPUItem.vue
@@ -3,16 +3,16 @@
     <!-- gpu-cores -->
     <div class="w-full flex">
       <span class="text-dimmer w-44">GPU count</span>
-      <span>{{ gpu_cores }}</span>
+      <span>{{ system.gpu?.cores }}</span>
     </div>
     <!-- gpu detail message -->
-    <div class="w-full flex" v-for="gpu in gpuList" :key="gpu.key">
+    <div class="w-full flex" v-for="(gpu, i) in system.gpu.memory" :key="i">
       <!-- gpu -->
-      <span class="text-dimmer w-44">{{ gpu.key }}</span>
+      <span class="text-dimmer w-44">GPU{{ i }}</span>
       <!-- type -->
-      <span class="w-96 shrink">{{ gpu.type }}</span>
+      <span class="w-96 shrink">{{ system.gpu.type[i] }}</span>
       <!-- memory -->
-      <span class="w-50 shrink-0">{{ gpu.memory }}</span>
+      <span class="w-50 shrink-0">{{ gpu }}</span>
     </div>
   </div>
 </template>
@@ -24,17 +24,20 @@
  * @since: 2024-01-25 22:52:50
  **/
 
-import { ref } from 'vue'
+import { useExperimentStroe } from '@swanlab-vue/store'
 
-const gpu_cores = ref(4)
+// const gpu_cores = ref(4)
+const experimentStore = useExperimentStroe()
+const experiment = experimentStore.experiment
+const system = experiment.system
 
 // 模拟数据
-const gpuList = [
-  { key: 'GPU0', type: 'NVIDIA GeForce RTX 3060 Laptop GPU', memory: '40.00GB' },
-  { key: 'GPU1', type: 'NVIDIA GeForce GTX 1660 Ti', memory: '32.00GB' },
-  { key: 'GPU2', type: 'AMD Radeon RX 6700 XT', memory: '16.00GB' },
-  { key: 'GPU3', type: 'AMD Radeon RX 6700 XT', memory: '16.00GB' }
-]
+// const gpuList = [
+//   { key: 'GPU0', type: 'NVIDIA GeForce RTX 3060 Laptop GPU', memory: '40.00GB' },
+//   { key: 'GPU1', type: 'NVIDIA GeForce GTX 1660 Ti', memory: '32.00GB' },
+//   { key: 'GPU2', type: 'AMD Radeon RX 6700 XT', memory: '16.00GB' },
+//   { key: 'GPU3', type: 'AMD Radeon RX 6700 XT', memory: '16.00GB' }
+// ]
 </script>
 
 <style lang="scss" scoped></style>
