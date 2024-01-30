d87457ee61f805c3bc63f3a9a5d53c45c3b5758e
KAAANG <cunykang@gmail.com>
2024-01-30 13:15:02 +0800

    Update _server.py

--- test/_server.py
+++ test/_server.py
@@ -1,3 +1,7 @@
+# from swanlab.env import ROOT
+# import os
+
+# os.environ[ROOT] = "/Users/likang/Code/swanhub-dev/swanlab/test/experiments"
 from swanlab.db import connect
 from swanlab.server.app import app
 
