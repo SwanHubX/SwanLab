0886b1a0e9e7604bb90189e9bfa63cee102dfced
KAAANG <cunykang@gmail.com>
2024-01-31 22:52:01 +0800

    single chart

--- vue/src/charts/components/PlayButton.vue
+++ vue/src/charts/components/PlayButton.vue
@@ -1,21 +1,7 @@
 <template>
-  <SLButton class="p-2 rounded-full">
-    <!-- TODO: 换换 -->
-    <svg
-      t="1706616480334"
-      class="icon"
-      viewBox="0 0 1024 1024"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      p-id="1483"
-      width="16"
-      height="16"
-    >
-      <path
-        d="M90.624 100.442028C90.624 8.302184 154.098115-26.506202 231.905095 21.611273l642.931359 401.320211c77.80698 49.14125 77.80698 128.995782 0 177.113256L231.905095 1002.388727C154.098115 1050.506202 90.624 1015.697816 90.624 923.557972V100.442028z"
-        p-id="1484"
-      ></path>
-    </svg>
+  <SLButton class="p-1 rounded border flex items-center justify-center" :style="{ color }" @click="handelPlay">
+    <SLIcon class="h-5 w-5" icon="pause" v-if="modelValue" />
+    <SLIcon class="h-5 w-5" icon="play" v-else />
   </SLButton>
 </template>
 
@@ -25,8 +11,30 @@
  * @file: PlayButton.vue
  * @since: 2024-01-30 21:20:05
  **/
+import { ref } from 'vue'
 
-// TODO: 状态 => 显示，目前还没有暂停等状态
+const props = defineProps({
+  color: {
+    type: String,
+    required: true
+  },
+  modelValue: {
+    type: Boolean,
+    required: true
+  }
+})
+
+const emit = defineEmits(['play', 'pause', 'update:modelValue'])
+
+const handelPlay = () => {
+  if (!props.modelValue) {
+    emit('play')
+    emit('update:modelValue', true)
+  } else {
+    emit('pause')
+    emit('update:modelValue', false)
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
