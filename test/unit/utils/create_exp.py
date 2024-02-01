efd2fb6641c600bbf25f6c069ad03c1bc129785b
KAAANG <cunykang@gmail.com>
2024-02-01 22:20:35 +0800

    update script

--- test/unit/utils/create_exp.py
+++ test/unit/utils/create_exp.py
@@ -5,7 +5,7 @@ r"""
 @File: test/unit/utils/create_exp.py
 @IDE: vscode
 @Description:
-    创建实验，请确保在unit目录下存在temp文件夹,用于保存实验数据
+    创建实验，会自动创建unit/temp目录，存放实验数据
 """
 import os
 
@@ -26,16 +26,33 @@ os.environ[ROOT] = save_dir
 print("set swanlog path: ", os.environ[ROOT])
 
 
-def run():
-    """运行实验"""
-    if not os.path.exists(save_dir):
-        os.mkdir(save_dir)
-    run = swanlab.init(log_level="debug")
-    # audio
-    sample_rate = 44100
-    test_audio_arr = np.random.randn(2, 100000)
-    run.log({"test-1": 1, "test-2": 1, "no": swanlab.Audio(test_audio_arr, sample_rate=sample_rate)})
+if not os.path.exists(save_dir):
+    os.mkdir(save_dir)
+run = swanlab.init(log_level="debug")
+# audio
+sample_rate = 44100
+test_audio_arr = np.random.randn(2, 100000)
+run.log({"test-1": 1, "test-2": 1, "no": swanlab.Audio(test_audio_arr, sample_rate=sample_rate)})
+swanlab.finish()
 
 
 if __name__ == "__main__":
-    run()
+    # 注册的实验名称
+    exp_name = run.settings.exp_name
+    # 检查写入的字段是否正确,不需要再次连接数据库
+    from swanlab.db import *
+
+    # 数据库中应该只有一个名为exp_name的实验
+    assert Experiment.select().where(Experiment.name == exp_name).count() == 1
+    # 获取这个实验
+    exp = Experiment.get(name=exp_name)
+    assert exp is not None
+    assert exp.run_id == run.settings.run_id
+    assert exp.name == exp_name
+    assert exp.status == 1
+    """
+    检查当前实验的tag字段数量
+    """
+    print(exp.tags)
+
+    print(exp)
