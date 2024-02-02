2936338a98a4eddc820654d7c54c0acee0ae5a9a
ZeYi Lin <944270057@qq.com>
2024-02-03 01:13:41 +0800

    update Image class: check invaild nparray

--- swanlab/data/modules/image.py
+++ swanlab/data/modules/image.py
@@ -13,19 +13,30 @@ class Image(BaseType):
     ----------
     data_or_path: (str or numpy.array or PIL.Image)
         Path to the image file, numpy array of image data or PIL.Image data.
+    mode: (str)
+        The PIL Mode for a image. Most commom is 'L', 'RGB', 'RGBA'.
+        More information about the mode can be found at https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-modes
     caption: (str)
         Caption for the image.
     """
 
-    def __init__(self, data_or_path: Union[str, np.ndarray, PILImage.Image, List["Image"]], caption: str = None):
+    def __init__(
+        self,
+        data_or_path: Union[str, np.ndarray, PILImage.Image, List["Image"]],
+        mode: str = "RGB",
+        caption: str = None,
+    ):
         super().__init__(data_or_path)
         self.image_data = None
+        self.mode = mode
         self.caption = self.__convert_caption(caption)
 
     def get_data(self):
         # 如果传入的是Image类列表
         if isinstance(self.value, list):
             return self.get_data_list()
+
+        # 图像预处理
         self.__preprocess(self.value)
         # 获取图像的hash值
         hash_name = get_file_hash_pil(self.image_data)[:16]
@@ -68,40 +79,32 @@ class Image(BaseType):
             image = self.__convert_numpy_array_to_image(data)
         elif isinstance(data, PILImage.Image):
             # 如果输入为PIL.Image
-            image = data
+            image = data.convert(self.mode)
         else:
             # 以上都不是，则报错
             raise TypeError("Unsupported image type. Please provide a valid path, numpy array, or PIL.Image.")
-
-        # 对数据做通道转换
-        image = self.__convert_channels(image)
-
         # 缩放大小
         image = self.__resize(image)
 
         self.image_data = image
 
     def __load_image_from_path(self, path):
-        """判断字符串是否为正确的图像路径，如果是则返回PIL.Image类型对象，如果不是则报错"""
+        """判断字符串是否为正确的图像路径，如果是则返回np.ndarray类型对象，如果不是则报错"""
         try:
-            return PILImage.open(path)
+            return PILImage.open(path).convert(self.mode)
         except Exception as e:
             raise TypeError(f"Invalid image path: {path}") from e
 
     def __convert_numpy_array_to_image(self, array):
-        """判断np array对象是否能转换为PIL.Image，如果是则返回PIL.Image类型对象，如果不是则报错"""
+        """ """
         try:
-            return PILImage.fromarray(array)
+            if array.ndim == 2 or (array.ndim == 3 and array.shape[2] in [3, 4]):
+                array = np.clip(array, 0, 255).astype(np.uint8)
+                return PILImage.fromarray(array, mode=self.mode)
+            else:
+                raise TypeError("Invalid numpy array: the numpy array must be 2D or 3D with 3 or 4 channels.")
         except Exception as e:
             raise TypeError("Invalid numpy array for the image") from e
-
-    def __convert_channels(self, image):
-        """将1通道和4通道图像转换为3通道的RGB图像。"""
-        if image.mode == "L":  # 1-channel
-            return image.convert("RGB")
-        elif image.mode == "RGBA" or image.mode == "CMYK":  # 4-channel
-            return image.convert("RGB")
-        return image
 
     def __resize(self, image, MAX_DIMENSION=1280):
         """将图像调整大小, 保证最大边长不超过MAX_DIMENSION"""
