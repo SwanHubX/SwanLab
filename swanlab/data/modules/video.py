e445cf126350d1fcfa55854c2d4d8ff199fbe57f
KashiwaByte <471314513@qq.com>
2024-02-21 13:48:29 +0800

    feat:add function __save and __prepare_video

--- swanlab/data/modules/video.py
+++ swanlab/data/modules/video.py
@@ -11,10 +11,12 @@ from .base import BaseType
 from ..utils.file import get_file_hash_numpy_array, get_file_hash_path
 import os
 from typing import Union, List
+import logging
 import numpy as np
 from io import BytesIO
-from typing import TextIO, Optional
+from typing import  Optional
 from moviepy.editor import VideoClip
+import numpy as np
 
 
 class Video(BaseType):
@@ -47,7 +49,7 @@ class Video(BaseType):
 
     def __init__(
         self,
-        data_or_path: Union[ str,"np.ndarray", "TextIO", "BytesIO",List["Video"]],
+        data_or_path: Union[ str,"np.ndarray", "BytesIO",List["Video"]],
         caption: Optional[str] = None,
         fps: int = 4,
         format: Optional[str] = None,
@@ -56,8 +58,9 @@ class Video(BaseType):
         self.caption = self.__convert_caption(caption)
         self.fps = fps
         self.format = format
-        self.width = None
+        self.time = None
         self.height = None
+        self.width = None
         self.channels = None
         if self.format not in Video.EXTS:
             raise ValueError("swanlab.Video accepts %s formats" % ", ".join(Video.EXTS))
@@ -67,7 +70,7 @@ class Video(BaseType):
         if isinstance(self.value, list):
             return self.get_data_list()
         # 视频预处理
-        self.__preprocess(self.value)
+        self.video_data = self.__preprocess(self.value)
         # 获取视频hash值
         hash_name = (
             get_file_hash_numpy_array(self.video_data)[:16]
@@ -90,7 +93,7 @@ class Video(BaseType):
         return save_name
 
     def expect_types(self, *args, **kwargs) -> list:
-        return ["str","np.ndarray", "TextIO", "BytesIO"]
+        return ["str","np.ndarray", "BytesIO"]
 
     def __convert_caption(self, caption):
         """将caption转换为字符串"""
@@ -109,6 +112,64 @@ class Video(BaseType):
 
     def __preprocess(self,data):
         """将不同类型的输入转换为"""
+        if isinstance(data,str):
+            # 如果输入为字符串
+           self.__load_video_from_path(data)
+        elif isinstance(data,np.ndarray):
+             # 如果输入为numpy array
+            tensor = self.__prepare_video(self.data)
+            self.time, self.height, self.width, self.channels = tensor.shape
+            return tensor
+        elif isinstance(data,BytesIO):
+            # 如果输入为二进制数据流
+            self.__load_video_from_BytesIO()
+            pass
+        else:
+            # 以上都不是，则报错
+            raise ValueError(
+                    "swanlab.Video accepts a file path or numpy like data as input"
+                )
+
+    def __load_video_from_path(self,path):
+        """判断字符串是否为正确的视频路径，如果是则返回np.ndarry类型对象，如果不是则报错"""
+        pass
+
+    def __load_video_from_BytesIO(self,Bytes):
+        """将字节流保存为临时视频文件，再读取为np.ndarry类型对象"""
+
+        pass
+
+    def __prepare_video(self, video: "np.ndarray") -> "np.ndarray":
+        """This logic was mostly taken from tensorboardX."""
+        if video.ndim < 4:
+            raise ValueError(
+                "Video must be atleast 4 dimensions: time, channels, height, width"
+            )
+        if video.ndim == 4:
+            video = video.reshape(1, *video.shape)
+        b, t, c, h, w = video.shape
+
+        if video.dtype != np.uint8:
+            logging.warning("Converting video data to uint8")
+            video = video.astype(np.uint8)
+
+        def is_power2(num: int) -> bool:
+            return num != 0 and ((num & (num - 1)) == 0)
+
+        # pad to nearest power of 2, all at once
+        if not is_power2(video.shape[0]):
+            len_addition = int(2 ** video.shape[0].bit_length() - video.shape[0])
+            video = np.concatenate(
+                (video, np.zeros(shape=(len_addition, t, c, h, w))), axis=0
+            )
+
+        n_rows = 2 ** ((b.bit_length() - 1) // 2)
+        n_cols = video.shape[0] // n_rows
+
+        video = np.reshape(video, newshape=(n_rows, n_cols, t, c, h, w))
+        video = np.transpose(video, axes=(2, 0, 4, 1, 5, 3))
+        video = np.reshape(video, newshape=(t, n_rows * h, n_cols * w, c))
+        return video
 
     def __save(self, save_path):
         """将视频保存到指定路径"""
@@ -118,8 +179,7 @@ class Video(BaseType):
         frame_count = write_video_data.shape[0]
         duration = frame_count / fps
         # 读取视频帧
-        frame = write_video_data[int(duration * fps)]
-        clip = VideoClip(frame, duration=duration)
+        clip = VideoClip(write_video_data, duration=duration)
         try:
             clip.write_videofile(save_path, fps=fps, codec="libx264")
         except Exception as e:
