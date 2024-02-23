1bd0fae4bc4747c9d9970a7a15b20b4160668aed
Zirui Cai <74649535+Feudalman@users.noreply.github.com>
2024-02-23 16:31:16 +0800

    feat: skeleton

--- vue/src/charts/modules/TextModule.vue
+++ vue/src/charts/modules/TextModule.vue
@@ -11,21 +11,10 @@
       <!-- body -->
       <div class="w-full h-[310px] overflow-y-auto border-b" :class="{ 'h-[60vh]': modal }">
         <!-- line -->
-        <div class="line" v-for="(text, i) in texts[tag][currentPage[index] - 1]" :key="text + i">
+        <div class="line" v-for="(text, i) in getTexts(tag, index)" :key="text + i" v-show="!skeleton">
           <!-- caption -->
-          <div
-            class="caption"
-            :title="
-              texts[tag][currentPage[index] - 1]?.length != 1
-                ? data[tag].list[currentPage - 1]?.more[i]
-                : data[tag].list[currentPage - 1]?.more
-            "
-          >
-            {{
-              (texts[tag][currentPage[index] - 1]?.length != 1
-                ? data[tag].list[currentPage - 1]?.more[i]
-                : data[tag].list[currentPage - 1]?.more) || '-'
-            }}
+          <div class="caption" :title="getCaption(tag, index, i)">
+            {{ getCaption(tag, index, i) }}
           </div>
           <!-- text -->
           <div class="text" :title="text">{{ text }}</div>
@@ -36,6 +25,15 @@
             @click="zoom(tag, i, text)"
           />
         </div>
+        <!-- 骨架 -->
+        <div v-if="skeleton || !getTexts(tag, index)">
+          <div class="flex items-center border-b border-dimmest" v-for="i in [1, 2, 3]" :key="i">
+            <div class="md:w-40 w-24 h-10 px-4 shrink-0 flex items-center border-r">
+              <span class="skeleton w-full h-1/2"></span>
+            </div>
+            <div class="flex items-center w-full h-10 ml-4"><span class="skeleton w-1/2 h-1/2"></span></div>
+          </div>
+        </div>
       </div>
       <SlideBar
         class="pt-2"
@@ -44,7 +42,7 @@
         :min="1"
         :bar-color="color"
         :key="totalPage[index]"
-        @change="$emit('getText', tag, currentPage[index])"
+        @change="turnPage(tag, currentPage[index])"
         v-if="currentPage != [] && totalPage[index] && totalPage[index] != 1"
       />
       <SLModal max-w="1200" v-model="isZoom">
@@ -64,6 +62,7 @@ import { ref, inject, computed } from 'vue'
 import SLModal from '@swanlab-vue/components/SLModal.vue'
 import TextDetail from './TextDetail.vue'
 import SlideBar from '../components/SlideBar.vue'
+import { debounce } from '@swanlab-vue/utils/common'
 
 const props = defineProps({
   data: {
@@ -83,11 +82,24 @@ const props = defineProps({
   }
 })
 
-defineEmits(['getText'])
+const emits = defineEmits(['getText'])
 
 const color = inject('colors')[0]
+const skeleton = ref(false)
 
-const getLine = () => {}
+const getCaption = (tag, index, i) => {
+  const texts = props.texts[tag]
+  const line = props.data[tag].list
+  const cp = currentPage.value[index] - 1
+  if (texts[cp]?.length == 1) {
+    return line[cp]?.more?.caption || '-'
+  }
+  return line[cp]?.more[i]?.caption || '-'
+}
+
+const getTexts = (tag, index) => {
+  return props.texts[tag][currentPage.value[index] - 1]
+}
 
 // ---------------------------------- 分页 ----------------------------------
 
@@ -98,6 +110,14 @@ const totalPage = computed(() => {
     return Math.ceil(props.data[tag].sum / pageSize.value)
   })
 })
+
+const turnPage = (tag, index) => {
+  skeleton.value = true
+  debounce(() => {
+    skeleton.value = false
+  }, 300)()
+  emits('getText', tag, index)
+}
 
 // ---------------------------------- 放大 ----------------------------------
 
@@ -119,7 +139,7 @@ const zoom = (tag, i, text) => {
 
 <style lang="scss" scoped>
 .caption {
-  @apply md:w-40 w-24 py-2 px-4 border-r truncate shrink-0;
+  @apply md:w-40 w-24 h-10 px-4 border-r truncate shrink-0 pt-2;
 }
 
 .text {
@@ -134,6 +154,45 @@ const zoom = (tag, i, text) => {
     .icon {
       @apply border border-dimmest rounded-sm hover:bg-highest hover:rounded block;
     }
+  }
+}
+
+@-webkit-keyframes skeleton-ani {
+  0% {
+    left: 0;
+  }
+
+  to {
+    left: 100%;
+  }
+}
+
+@keyframes skeleton-ani {
+  0% {
+    left: 0;
+  }
+
+  to {
+    left: 100%;
+  }
+}
+
+.skeleton {
+  @apply block;
+  background-image: linear-gradient(-45deg, #f5f5f5 40%, #fff 55%, #f5f5f5 63%);
+  list-style: none;
+  background-size: 400% 100%;
+  background-position: 100% 50%;
+  animation: skeleton-animation 2s ease infinite;
+}
+
+@keyframes skeleton-animation {
+  0% {
+    background-position: 100% 50%;
+  }
+
+  100% {
+    background-position: 0 50%;
   }
 }
 </style>
