e1b3bc787b355b19b0554b8f05c82e92003d2ed7
KAAANG <cunykang@gmail.com>
2024-02-27 16:14:45 +0800

    legend component

---
+++ vue/src/charts/components/LineChartLegend.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="lc-legend">
+    <div
+      class="lc-legend-item"
+      :style="{ color: item.color }"
+      v-for="item in items"
+      :key="item.name"
+      @mouseenter="handelMouseenter(item)"
+      @mouseleave="handelMouseleave(item)"
+      @click="handelClick(item)"
+    >
+      <RouterLink :to="'/experiment/' + item.experiment_id">
+        {{ item.name }}
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+/**
+ * @description: 折线图图例组件
+ * @file: LineChartLegend.vue
+ * @since: 2024-02-27 15:02:13
+ **/
+
+defineProps({
+  items: {
+    type: Array,
+    default: () => []
+  }
+})
+// console.log('items', props.items)
+
+const emit = defineEmits(['hoverin', 'hoverout'])
+
+// ---------------------------------- 悬浮事件 ----------------------------------
+const handelMouseenter = (item) => {
+  emit('hoverin', item)
+}
+const handelMouseleave = (item) => {
+  emit('hoverout', item)
+}
+
+// ---------------------------------- 点击事件 ----------------------------------
+const handelClick = (item) => {
+  emit('click', item)
+}
+</script>
+
+<style lang="scss" scoped>
+.lc-legend {
+  max-height: 28px;
+  min-height: 16px;
+  @apply overflow-y-auto flex flex-wrap px-3 mb-1 gap-y-0.5 gap-x-2 leading-none text-[12px];
+  @apply justify-center;
+  .lc-legend-item {
+    @apply flex flex-shrink-0 items-center hover:brightness-75;
+    &:before {
+      content: '';
+      display: inline-block;
+      width: 8px;
+      height: 2px;
+      margin-right: 4px;
+      background-color: currentColor;
+    }
+  }
+}
+</style>
