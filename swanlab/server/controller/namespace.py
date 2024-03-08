eb95c1807b587368d53a25c3f4a0a860aa76fcff
Zachary_Yang <2431542525@qq.com>
2024-03-08 17:34:01 +0800

    feat(app.py): add namespace router

--- swanlab/server/controller/namespace.py
+++ swanlab/server/controller/namespace.py
@@ -5,7 +5,6 @@ from ..module.resp import SUCCESS_200, DATA_ERROR_500, CONFLICT_409, NOT_FOUND_4
 from fastapi import Request
 
 import yaml
-from ...log import swanlog
 from typing import List, Dict
 
 from ...db import connect, NotExistedError
@@ -20,7 +19,8 @@ __to_list = Namespace.search2list
 
 # 修改namespace下可见性
 def change_namespace_visibility(namespace_id: int, opened: bool):
+    # 参数校验
     namespace = Namespace.get_by_id(namespace_id)
     namespace.opened = opened
     namespace.save()
-    return SUCCESS_200({"namespace": namespace_id})
+    return SUCCESS_200({"namespace": namespace.__dict__})
