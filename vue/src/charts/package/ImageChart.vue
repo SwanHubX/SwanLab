2ee5199ee009575355fac0c3904a17c03c6ea53c
Zirui Cai <74649535+Feudalman@users.noreply.github.com>
2024-03-13 17:55:54 +0800

    Update ImageChart.vue (#410)

--- vue/src/charts/package/ImageChart.vue
+++ vue/src/charts/package/ImageChart.vue
@@ -573,7 +573,7 @@ defineExpose({
 .image-content {
   @apply mt-1 p-2 w-full rounded-sm relative min-h-[224px];
   img {
-    @apply w-[160px] cursor-pointer;
+    @apply min-w-[160px] cursor-pointer;
   }
   .images-container {
     @apply grid gap-2 h-full;
