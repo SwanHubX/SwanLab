5cc8d73eba724fc6cccb45c06979c046740f7cf5
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-04-01 15:04:45 +0800

    reformat file

--- swanlab/data/modules/__init__.py
+++ swanlab/data/modules/__init__.py
@@ -4,13 +4,12 @@ from .image import Image
 from .text import Text
 from .video import Video
 from .object_3d import Object3D
-
 # from .video import Video
-from typing import Protocol, Union
+from typing import Protocol, Union, List
 
 
 class FloatConvertible(Protocol):
     def __float__(self) -> float: ...
 
 
-DataType = Union[float, FloatConvertible, int, BaseType]
+DataType = Union[float, FloatConvertible, int, BaseType, List[BaseType]]
