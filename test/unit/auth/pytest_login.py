9a3404b1bbb57a8f8bf7831dfb77cc77eb850b70
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-04-03 16:33:21 +0800

    update

--- test/unit/auth/pytest_login.py
+++ test/unit/auth/pytest_login.py
@@ -8,3 +8,12 @@ r"""
     测试登录
 """
 from swanlab.auth.login import login_by_key
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_login_success():
+    """
+    测试登录成功
+    """
+    assert 1 == 1
