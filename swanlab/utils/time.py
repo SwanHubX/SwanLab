4061abe577b7d3f9d8c0f9826bfc61d33ae6c8b3
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-04-10 19:25:35 +0800

    fix upload bug

--- swanlab/utils/time.py
+++ swanlab/utils/time.py
@@ -1,6 +1,14 @@
 from datetime import datetime
+import pytz
 
 
 def create_time():
     """获取当前时间"""
-    return datetime.utcnow().isoformat()
+    # 获取当前时间
+    current_time = datetime.utcnow()
+    # 获取UTC时区对象
+    utc_timezone = pytz.utc
+    # 将当前时间转换为带有时区信息的时间
+    current_time_with_timezone = utc_timezone.localize(current_time)
+    # 返回带有时区信息的当前时间
+    return current_time_with_timezone.isoformat()
