541abfeb32aa23e64bc31c55e3b1d9ab0a8121e5
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-04-23 16:24:18 +0800

    refactor chart

--- vue/src/docs/chart.js
+++ vue/src/docs/chart.js
@@ -4,13 +4,11 @@
 // 开源版对象类型以Open为前缀
 
 /**
- * @description 一个完整的图表配置，包含了当前页面的所有图表信息
- * @typedef {OpenSection[]} OpenSections
+ * @typedef {OpenSection[]} OpenSections 一个完整的图表配置，包含了当前页面的所有图表信息
  */
 
 /**
- * @description 一个节容器，存放图表，和一些默认情况
- * @typedef {Object} OpenSection
+ * @typedef {Object} OpenSection 一个节容器，存放图表，和一些默认情况
  * @property {number | string} id 平常不会使用，用于区别pin的section（-1）和hidden的section（-2），不影响排序，只影响名称显示；另一方面，将使用这个作为v-for时的key
  * @property {string} name section的名称，特别的，如果为default或者id为-1、-2，则按照前端定义的名称显示
  * @property {boolean | number} opened section是否展开，默认为true或者1
@@ -18,8 +16,7 @@
  */
 
 /**
- * @description 一个图表配置，用于标注这个图表是什么类型以及配置是什么，使用了什么数据源
- * @typedef {Object} OpenChart
+ * @typedef {Object} OpenChart 一个图表配置，用于标注这个图表是什么类型以及配置是什么，使用了什么数据源
  * @property {number | string} id 图表唯一id，用于v-for时的key
  * @property {boolean} multi 标注是否为多实验图表显示，这涉及到图表组件内部不同的数据处理方式
  * @property {string} name 图表名称
@@ -28,4 +25,22 @@
  * @property {Object} source_map 数据源映射，用于前端显示数据源的名称映射到数据源对应的实验id
  * @property {Object} error 当前图表是否错误，如果不为null说明图表错误，此信息用于显示错误
  * @property {string} error.data_class sdk中上传时的数据类型，用于前端提示
+ */
+
+/**
+ *
+ */
+
+/**
+ * @callback OpenChartSubscribe 图表本身向父组件传递数据的订阅函数
+ * @param {string[]} sources 数据源名称
+ * @param {string} chartId 图表id
+ * @param {OpenChartSubscribeCallback} callback 订阅成功后的回调函数
+ */
+
+/**
+ * @callback OpenChartSubscribeCallback 图表本身向父组件传递数据的 订阅成功/数据更改 的回调函数，父组件将在订阅成功后调用这个函数
+ * @param {string} key 数据源名称
+ * @param {string} data 数据源的数据
+ * @param {Obejct} error 请求失败的错误信息——如果有的话，没有就是null
  */
