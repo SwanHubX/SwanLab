88392fba24b9d4987e49fc9d0f9d7fed5c80f7e9
ZeYi Lin <944270057@qq.com>
2024-04-23 16:44:19 +0800

    first try

--- swanlab/__init__.py
+++ swanlab/__init__.py
@@ -1,4 +1,5 @@
 # 导出初始化函数和log函数
+import swanlab.jupyter
 from .data import (
     login,
     init,
@@ -12,5 +13,10 @@ from .data import (
 )
 
 from .package import get_package_version
+
+
+def load_ipython_extension(ipython):
+    ipython.register_magics(swanlab.jupyter.SwanLabMagics)
+
 
 __version__ = get_package_version()
