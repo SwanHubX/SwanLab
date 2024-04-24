7fdda45447c860712b1ab7c1ef5fdb00b0441663
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-04-24 21:02:10 +0800

    fix search bug

--- vue/src/charts/ChartsDashboard.vue
+++ vue/src/charts/ChartsDashboard.vue
@@ -37,6 +37,7 @@ import { t } from '@swanlab-vue/i18n'
 import SmoothButton from './components/SmoothButton.vue'
 import { debounces } from '@swanlab-vue/utils/common'
 import SLSearch from '@swanlab-vue/components/SLSearch.vue'
+
 const props = defineProps({
   // 整个图表列表集合
   groups: {
@@ -76,7 +77,8 @@ const props = defineProps({
   // 取消订阅数据更新
   unsubscribe: {
     type: Function,
-    default: () => {}
+    default: () => {
+    }
   }
 })
 const chartsGroupList = ref(props.groups)
@@ -186,18 +188,21 @@ const unhide = (chart) => {
 // ---------------------------------- 处理搜索图表事件 ----------------------------------
 const search = (value) => {
   if (!value) return (searchChartsGroupList.value = null)
-  const newGroup = chartsGroupList.value.filter((group) => {
-    return group.charts.some((chart) => {
-      return chart.name.toLowerCase().includes(value.toLowerCase())
-    })
-  })
-  // 然后将newGroup中滤除不包含value的chart
-  newGroup.forEach((group) => {
+  // newGroup中滤除不包含value的chart，需要注意的是不能直接修改原数组，否则会影响到原数组
+  const newGroups = []
+  for (const group of chartsGroupList.value) {
+    if (group.charts.some((chart) => chart.name.toLowerCase().includes(value.toLowerCase()))) {
+      newGroups.push({ ...group, charts: [...group.charts] })
+    }
+  }
+  newGroups.forEach((group) => {
+    group.charts = [...group.charts]
     group.charts = group.charts.filter((chart) => {
       return chart.name.toLowerCase().includes(value.toLowerCase())
     })
   })
-  searchChartsGroupList.value = newGroup
+
+  searchChartsGroupList.value = newGroups
 }
 
 // ---------------------------------- 依赖注入 ----------------------------------
