222ccaec416b8d326361939a5efa53d1777be88e
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-06-05 00:47:20 +0800

    update test

--- test/unit/data/run/pytest_main.py
+++ test/unit/data/run/pytest_main.py
@@ -21,6 +21,7 @@ import numpy as np
 import pytest
 import random
 import os
+import io
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -92,11 +93,13 @@ class TestSwanLabRunState:
 
 class TestSwanLabRunLog:
     """
-    测试SwanLabRun的日志解析功能，不包含操作员
+    测试SwanLabRun的日志解析功能，不包含操作员，由于情况比较多，这里分情况测试
+    1. 一开始正常输入，然后重复输入，然后输入新的key的重复的step
+    2. 一开始输入错误的类型，然后正常输入
+    3. 一开始输入正确的类型，然后输入错误的类型
     """
 
-    # ---------------------------------- 解析log数字 ----------------------------------
-
+    # ---------------------------------- 解析log数字/Line ----------------------------------
     def test_log_number_ok(self):
         run = SwanLabRun()
         data = {"a": 1, "b": 0.1, "math.nan": math.nan, "math.inf": math.inf}
@@ -108,6 +111,7 @@ class TestSwanLabRunLog:
         assert ll["b"].data == 0.1
         assert ll["math.nan"].data == Line.nan
         assert ll["math.inf"].data == Line.inf
+        assert all([ll[k].column_info.chart == ll[k].column_info.chart.LINE for k in ll])
         # 没有其他多余的内容
         assert all([ll[k].buffers is None for k in ll])
         assert all([ll[k].data is not None for k in ll])
@@ -166,3 +170,107 @@ class TestSwanLabRunLog:
 
         assert ll["a"].error_info is None
         assert ll["b"].error_info is None
+
+    # ---------------------------------- 解析log文字 ----------------------------------
+
+    def test_log_text_ok(self):
+        run = SwanLabRun()
+        data = {"a": Text("abc"), "b": Text("def")}
+        ll = run.log(data)
+        assert len(ll) == 2
+        assert all([ll[k].error is False for k in ll])
+        assert ll["a"].data == ["abc"]
+        assert ll["b"].data == ["def"]
+        assert all([ll[k].column_info.chart == ll[k].column_info.chart.TEXT for k in ll])
+        assert all([ll[k].buffers is None for k in ll])
+        assert all([ll[k].data is not None for k in ll])
+        assert all([ll[k].step == 0 for k in ll])
+        ll2 = run.log(data, step=3)
+        assert all(ll2[k].step == 3 for k in ll2)
+        # list
+        ll3 = run.log({"a": [Text("abc"), Text("def")]})
+        assert ll3["a"].data == ["abc", "def"]
+
+    # ---------------------------------- 解析log Audio ----------------------------------
+
+    def test_log_audio_path(self):
+        """
+        通过路径添加音频
+        """
+        run = SwanLabRun()
+        # 正确的音频路径
+        path = os.path.join(TEMP_PATH, "test.wav")
+        samplerate = 5000
+        sf.write(path, np.random.rand(1000), samplerate)
+        data = {"a": Audio(path)}
+        ll = run.log(data)
+        assert len(ll["a"].buffers) == 1
+        buffer = ll["a"].buffers[0]
+        # 通过字节流解码后的采样点数
+        audio, _samplerate = sf.read(io.BytesIO(buffer.getvalue()))
+        assert _samplerate == samplerate
+
+    def test_log_audio_numpy(self):
+        """
+        通过numpy数组添加音频
+        """
+        run = SwanLabRun()
+        samplerate = 5000
+        data = {"a": Audio(np.random.rand(1000), samplerate)}
+        ll = run.log(data)
+        assert len(ll["a"].buffers) == 1
+        buffer = ll["a"].buffers[0]
+        # 通过字节流解码后的采样点数
+        audio, _samplerate = sf.read(io.BytesIO(buffer.getvalue()))
+        assert _samplerate == samplerate
+        # FIXME 一维的？
+        assert len(audio.shape) == 1
+
+    def test_log_audio_error(self):
+        """
+        错误的情况
+        """
+        run = SwanLabRun()
+        # 错误的路径
+        with pytest.raises(ValueError):
+            run.log({"a": Audio("error.wav")})
+        # 错误的类型
+        with pytest.raises(TypeError):
+            run.log({"a": Audio(1)})  # noqa
+        # 错误的矩阵编码类型
+        with pytest.raises(TypeError):
+            run.log({"a": Audio(np.random.rand(1000).astype(np.int8))})
+        # 错误的矩阵维数
+        with pytest.raises(TypeError):
+            run.log({"a": Audio(np.random.rand(1000, 5))})
+
+    # ---------------------------------- 图像 ----------------------------------
+
+    def test_log_image_path(self):
+        """
+        通过路径添加图像
+        """
+        run = SwanLabRun()
+        # 正确的图像路径
+        path = os.path.join(TEMP_PATH, "test.png")
+        PILImage.new("RGB", (100, 100)).save(path)
+        data = {"a": Image(path)}
+        ll = run.log(data)
+        assert len(ll["a"].buffers) == 1
+        buffer = ll["a"].buffers[0]
+        # 通过字节流解码后的图像
+        image = PILImage.open(io.BytesIO(buffer.getvalue()))
+        assert image.size == (100, 100)
+
+    def test_log_image_numpy(self):
+        """
+        通过numpy数组添加图像
+        """
+        run = SwanLabRun()
+        data = {"a": Image(np.random.rand(100, 100, 3))}
+        ll = run.log(data)
+        assert len(ll["a"].buffers) == 1
+        buffer = ll["a"].buffers[0]
+        # 通过字节流解码后的图像
+        image = PILImage.open(io.BytesIO(buffer.getvalue()))
+        assert image.size == (100, 100)
