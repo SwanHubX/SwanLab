e270f7d09343d1015f537e245c778e046c518ac3
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-06-04 17:30:53 +0800

    Update base.py

--- swanlab/data/modules/base.py
+++ swanlab/data/modules/base.py
@@ -221,7 +221,7 @@ class MediaBuffer(BytesIO):
             raise TypeError(f"Expected str, but got {type(value)}")
         if self.__file_name is not None:
             # 此时意味着使用类似 [] * 2 的操作复制了多个相同的实例，这允许，但不推荐
-            swanlog.warning("You are log duplicate and same instance this is not recommended")
+            swanlog.warning("You are log duplicate and same instance, this is not recommended")
         self.__file_name = value
 
 
