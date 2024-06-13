53ccc675c7f31c60dd27a029795f6f527226a0a0
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-06-13 14:41:14 +0800

    update watch test

--- swanlab/data/sdk.py
+++ swanlab/data/sdk.py
@@ -24,6 +24,7 @@ from swanlab.log import swanlog
 from swanlab.utils import check_load_json_yaml, check_proj_name_format
 from swanlab.api import code_login
 from swanlab.package import version_limit
+from swanboard import SwanBoardCallback
 
 
 def _check_proj_name(name: str) -> str:
@@ -68,16 +69,16 @@ def login(api_key: str = None):
 
 
 def init(
-    project: str = None,
-    workspace: str = None,
-    experiment_name: str = None,
-    description: str = None,
-    config: Union[dict, str] = None,
-    logdir: str = None,
-    suffix: Union[str, None, bool] = "default",
-    mode: Literal["disabled", "cloud", "local"] = None,
-    load: str = None,
-    **kwargs,
+        project: str = None,
+        workspace: str = None,
+        experiment_name: str = None,
+        description: str = None,
+        config: Union[dict, str] = None,
+        logdir: str = None,
+        suffix: Union[str, None, bool] = "default",
+        mode: Literal["disabled", "cloud", "local"] = None,
+        load: str = None,
+        **kwargs,
 ) -> SwanLabRun:
     """
     Start a new run to track and log. Once you have called this function, you can use 'swanlab.log' to log data to
@@ -294,6 +295,5 @@ def _create_operator(mode) -> Tuple[SwanLabRunOperator, Optional[CloudRunCallbac
         swanlog.warning("SwanLab run disabled, the data will not be saved or uploaded.")
         return SwanLabRunOperator(), None
     c = CloudRunCallback() if mode == SwanLabMode.CLOUD.value else LocalRunCallback()
-    from swanboard import SwanBoardCallback
     callbacks = [c, SwanBoardCallback()]
     return SwanLabRunOperator(callbacks), c
--- test/unit/cli/pytest_watch.py
+++ test/unit/cli/pytest_watch.py
@@ -7,15 +7,74 @@ r"""
 @Description:
     测试cli的watch命令
 """
+import os
 from swanlab.cli import cli
 from click.testing import CliRunner
+import multiprocessing
+import requests
+import swanlab
+from tutils import TEMP_PATH
+from swanlab.env import get_swanlog_dir, reset_env
 
 
+def mock_swanlog(path=None):
+    if path is None:
+        path = get_swanlog_dir()
+    reset_env()
+    swanlab.init(logdir=path, mode="local")
+    swanlab.log({"test": 1})
+    swanlab.finish()
+
+
+# 运行任务
 # noinspection PyTypeChecker
+def runner_watch(args=None):
+    if args is None:
+        args = []
+    runner = CliRunner()
+    runner.invoke(cli, ["watch", *args])
+
+
+# 测试能否ping通
+def ping(host="127.0.0.1", port=5092):
+    url = f"http://{host}:{port}"  # noqa
+    response = requests.get(url, timeout=10)
+    assert response.status_code == 200
+
+
 def test_watch_default():
     """
     测试watch命令的默认情况
+    由于全局解释器锁的存在，无法在同一个进程中同时运行两个线程，所以这里使用多进程进行测试
     """
-    runner = CliRunner()
-    result = runner.invoke(cli, ["watch"])
-    pass
+
+    # 运行任务之前需保证已经存在swanlog文件夹
+    mock_swanlog()
+
+    # 运行任务
+    p1 = multiprocessing.Process(target=runner_watch)
+    p2 = multiprocessing.Process(target=ping)
+    p1.start()
+    p2.start()
+    p2.join()
+    p1.kill()
+
+
+def test_watch_logdir():
+    """
+    测试watch命令的logdir参数
+    由于全局解释器锁的存在，无法在同一个进程中同时运行两个线程，所以这里使用多进程进行测试
+    """
+    # 新建一个logdir
+    logdir = os.path.join(TEMP_PATH, "watch", "swanlog")
+    os.makedirs(logdir, exist_ok=True)
+    # 运行任务之前需保证已经存在swanlog文件夹
+    mock_swanlog(logdir)
+
+    # 运行任务
+    p1 = multiprocessing.Process(target=runner_watch, args=["--logdir", logdir])
+    p2 = multiprocessing.Process(target=ping)
+    p1.start()
+    p2.start()
+    p2.join()
+    p1.kill()
