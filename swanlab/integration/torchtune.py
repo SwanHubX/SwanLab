6546c20c5976ce6c814451f589618db10ea5805c
ZeYi Lin <944270057@qq.com>
2024-06-22 01:03:28 +0800

    Update torchtune.py

--- swanlab/integration/torchtune.py
+++ swanlab/integration/torchtune.py
@@ -1,9 +1,14 @@
+"""
+swanlab.integration.torchtune.SwanLabLogger
+"""
+
 from pathlib import Path
 from typing import Mapping, Optional
 from omegaconf import DictConfig, OmegaConf
 from torchtune.utils.metric_logging import MetricLoggerInterface, Scalar, get_world_size_and_rank
 import swanlab
 import os
+from torchtune.utils.metric_logging import WandBLogger
 
 
 class SwanLabLogger(MetricLoggerInterface):
@@ -20,9 +25,6 @@ class SwanLabLogger(MetricLoggerInterface):
 
         self._swanlab = swanlab
 
-        # Use dir if specified, otherwise use log_dir.
-        self.log_dir = kwargs.pop("dir", logdir)
-
         _, self.rank = get_world_size_and_rank()
 
         if self._swanlab.get_run() is None and self.rank == 0:
@@ -31,20 +33,12 @@ class SwanLabLogger(MetricLoggerInterface):
                 workspace=workspace,
                 experiment_name=experiment_name,
                 description=description,
-                logdir=self.log_dir,
+                logdir=logdir,
                 mode=mode,
                 **kwargs,
             )
 
     def log_config(self, config: DictConfig) -> None:
-        """Saves the config locally and also logs the config to W&B. The config is
-        stored in the same directory as the checkpoint. You can
-        see an example of the logged config to W&B in the following link:
-        https://wandb.ai/capecape/torchtune/runs/6053ofw0/files/torchtune_config_j67sb73v.yaml
-
-        Args:
-            config (DictConfig): config to log
-        """
         if self._swanlab.get_run():
             resolved = OmegaConf.to_container(config, resolve=True)
             self._swanlab.config.update(resolved)
@@ -60,7 +54,7 @@ class SwanLabLogger(MetricLoggerInterface):
 
     def log(self, name: str, data: Scalar, step: int) -> None:
         if self._swanlab.get_run():
-            self._swanlab.log({name: data, "global_step": step})
+            self._swanlab.log({name: data}, step=step)
 
     def log_dict(self, payload: Mapping[str, Scalar], step: int) -> None:
         if self._swanlab.get_run():
