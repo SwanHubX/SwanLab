f465fe76d587a0616e61325aabb580218f20543d
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-08-18 13:08:04 +0800

    change action

--- build_pypi.py
+++ build_pypi.py
@@ -9,10 +9,21 @@ r"""
 """
 import subprocess
 import shutil
+import json
 import os
 
 # 如果dist文件夹存在则删除
 if os.path.exists("dist"):
     shutil.rmtree("dist")
+
+# 设置版本号
+version = os.getenv("VERSION")
+if not version:
+    raise ValueError("尚未指定构建版本号")
+with open("swanlab/package.json", 'r+') as f:
+    p = json.load(f)
+    p["version"] = version
+    json.dump(p, f, indent=4)
+
 # 构建python项目
 subprocess.run("python -m build", shell=True)
