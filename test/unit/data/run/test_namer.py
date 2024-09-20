55b082dfd047c27b06e92cd34ade2b2891f74030
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-09-20 14:51:05 +0800

    Update test_namer.py

--- test/unit/data/run/test_namer.py
+++ test/unit/data/run/test_namer.py
@@ -11,11 +11,16 @@ from swanlab.data.run import namer
 
 
 def test_name_no_index():
-    pass
+    name = namer.generate_name()
+    assert isinstance(name, str)
 
 
 def test_name_with_index():
-    pass
+    name = namer.generate_name(4)
+    assert isinstance(name, str)
+    # 极大数
+    name = namer.generate_name(999999999)
+    assert isinstance(name, str)
 
 
 def test_color_no_index():
