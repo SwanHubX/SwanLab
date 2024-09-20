79be53929fe91f0d42b3300057962b9d8a8ef1bc
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2024-09-20 14:42:51 +0800

    update:namer

--- swanlab/data/run/namer.py
+++ swanlab/data/run/namer.py
@@ -7,3 +7,22 @@ r"""
 @Description:
     命名器、取色器
 """
+from typing import Tuple, Optional
+
+
+def generate_name(index: Optional[int] = None) -> str:
+    """
+    生成名称
+    :param index: 生成名称的索引，约定为历史实验数，可以为None
+    :return: 生成的名称
+    """
+    pass
+
+
+def generate_color(index: Optional[int] = None) -> Tuple[str, str]:
+    """
+    生成颜色
+    :param index: 生成颜色的索引，约定为历史实验数，可以为None
+    :return: 生成的颜色，(白天颜色，夜晚颜色)
+    """
+    pass
---
+++ test/unit/data/run/test_namer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""
+@DATE: 2024/9/20 14:42
+@File: test_namer.py
+@IDE: pycharm
+@Description:
+    测试命名器、取色器
+"""
