d9fb3f6aac7dd2f746c36b5904aaf890cd01c13b
ZeYi Lin <944270057@qq.com>
2025-01-20 16:56:40 +0800

    add test

--- test/tensorboard_sync.py
+++ test/tensorboard_sync.py
@@ -1,20 +1,15 @@
-# from tensorboardX import SummaryWriter
-from torch.utils.tensorboard import SummaryWriter
-import swanlab
+from tensorboardX import SummaryWriter
+# from torch.utils.tensorboard import SummaryWriter
 import numpy as np
+import swanlab
 
-swanlab.sync_tensorboard_torch()
+swanlab.sync_tensorboardX()
+# swanlab.sync_tensorboard_torch()
 
-# 创建一个SummaryWriter对象，指定日志目录
 writer = SummaryWriter('runs/example')
 
-# 生成一些示例数据
 for i in range(100):
-    # 生成一个随机的标量值
     scalar_value = np.random.rand()
-    
-    # 记录标量值
     writer.add_scalar('random_scalar', scalar_value, i)
 
-# 关闭SummaryWriter
 writer.close()
