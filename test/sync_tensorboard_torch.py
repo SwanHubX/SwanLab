6a9c93e238aaf5950b6dca4c728eb4c21f0bea62
Ze-Yi LIN <58305964+Zeyi-Lin@users.noreply.github.com>
2025-01-21 18:06:26 +0800

    feat: sync enhancement (#799)

--- test/sync_tensorboard_torch.py
+++ test/sync_tensorboard_torch.py
@@ -11,8 +11,9 @@ from torch.utils.tensorboard import SummaryWriter
 import swanlab
 
 swanlab.sync_tensorboard_torch()
-
 writer = SummaryWriter('runs/example')
+
+writer.add_image('random_image', np.random.randint(0, 255, (3, 100, 100)), global_step=20)
 
 for i in range(100):
     scalar_value = np.random.rand()
