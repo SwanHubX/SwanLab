0b03cd8b0b55874781c4dc150c06e7ad5824467b
ZeYi Lin <944270057@qq.com>
2025-03-04 00:43:58 +0800

    add timeout

--- swanlab/data/run/metadata/conda.py
+++ swanlab/data/run/metadata/conda.py
@@ -16,7 +16,7 @@ def get_conda() -> Optional[str]:
     获取conda信息，如果不存在则返回None
     Returns: str
     """
-    result = subprocess.run(["conda env export"], shell=True, capture_output=True, text=True)
+    result = subprocess.run(["conda env export"], shell=True, capture_output=True, text=True, timeout=15)
     if result.returncode != 0:
         return None
     output = result.stdout
