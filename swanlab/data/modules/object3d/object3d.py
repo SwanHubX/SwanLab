2c3fd6388c5a6307189fc812edde0f4003641ad7
xj63 <xj63work@163.com>
2025-03-04 21:26:42 +0800

    feat(object3d): Add optional parameters to Object3D constructor

--- swanlab/data/modules/object3d/object3d.py
+++ swanlab/data/modules/object3d/object3d.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Any, Callable, Dict, Type, Union
+from typing import Any, Callable, Dict, Optional, Type, Union
 
 from swankit.core.data import MediaType
 
@@ -12,16 +12,26 @@ except ImportError:
 
 
 class Object3D:
-    def __new__(cls, data: Union[np.ndarray, str, Path]) -> MediaType:
+    def __new__(
+        cls,
+        data: Union[np.ndarray, str, Path],
+        *,
+        step: Optional[int] = None,
+        caption: Optional[str] = None,
+        **kwargs,
+    ) -> MediaType:
         cls._check_numpy()
 
+        kwargs['step'] = step
+        kwargs['caption'] = caption
+
         if isinstance(data, np.ndarray):
-            return cls._handle_ndarray(data)
+            return cls._handle_ndarray(data, **kwargs)
 
         if isinstance(data, (str, Path)):
-            return cls._handle_file(Path(data))
+            return cls._handle_file(Path(data), **kwargs)
 
-        return cls._handle_data(data)
+        return cls._handle_data(data, **kwargs)
 
     @staticmethod
     def _check_numpy() -> None:
@@ -29,7 +39,7 @@ class Object3D:
             raise ImportError("Numpy is required for Object3D class. " "Please install it with: pip install numpy.")
 
     @classmethod
-    def _handle_ndarray(cls, data: np.ndarray) -> MediaType:
+    def _handle_ndarray(cls, data: np.ndarray, **kwargs) -> MediaType:
         point_cloud_channel_handlers = {
             3: PointCloud.from_xyz,  # xyz
             4: PointCloud.from_xyzc,  # xyzc
@@ -37,7 +47,7 @@ class Object3D:
         }
 
         if data.ndim == 2 and data.shape[1] in point_cloud_channel_handlers:
-            return point_cloud_channel_handlers[data.shape[1]](data)
+            return point_cloud_channel_handlers[data.shape[1]](data, **kwargs)
 
         raise ValueError(
             f"Unsupported array format: shape={data.shape}. "
@@ -45,16 +55,16 @@ class Object3D:
         )
 
     _FILE_HANDLERS: Dict[str, Callable] = {
-        '.txt': lambda p: PointCloud.from_file(p),
-        '.xyz': lambda p: PointCloud.from_file(p),
-        '.pts': lambda p: PointCloud.from_file(p),
-        '.ply': lambda p: PointCloud.from_file(p),
-        '.pcd': lambda p: PointCloud.from_file(p),
+        # '.txt': lambda p: PointCloud.from_file(p),
+        # '.xyz': lambda p: PointCloud.from_file(p),
+        # '.pts': lambda p: PointCloud.from_file(p),
+        # '.ply': lambda p: PointCloud.from_file(p),
+        # '.pcd': lambda p: PointCloud.from_file(p),
         '.swanlab.pts.json': lambda p: PointCloud.from_file(p),
     }
 
     @classmethod
-    def _handle_file(cls, path: Path) -> MediaType:
+    def _handle_file(cls, path: Path, **kwargs) -> MediaType:
         if not path.exists():
             raise FileNotFoundError(f"File not found: {path}")
 
@@ -71,9 +81,11 @@ class Object3D:
             handler = cls._FILE_HANDLERS.get(suffix)
             if handler is not None:
                 try:
-                    return handler(path)
+                    return handler(path, **kwargs)
                 except Exception as e:
-                    raise ValueError(f"Error processing file {path} with handler for {suffix}: {str(e)}") from e
+                    raise ValueError(
+                        f"Error processing file {path} with handler for {suffix}: {str(e)}",
+                    ) from e
 
         raise ValueError(
             f"Unsupported file type: {path.name}\n"
@@ -88,10 +100,10 @@ class Object3D:
     }
 
     @classmethod
-    def _handle_data(cls, data: Any) -> MediaType:
+    def _handle_data(cls, data: Any, **kwargs) -> MediaType:
         handler = cls._TYPE_HANDLERS.get(type(data))
 
         if handler is not None:
-            return handler(data)
+            return handler(data, **kwargs)
 
         raise TypeError(f"Unsupported input type: {type(data)}")
