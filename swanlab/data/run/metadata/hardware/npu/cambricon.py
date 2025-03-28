d38be1551d76ee8a3066689152769e570591d45e
ZeYi Lin <944270057@qq.com>
2025-03-28 17:26:38 +0800

    update

--- swanlab/data/run/metadata/hardware/npu/cambricon.py
+++ swanlab/data/run/metadata/hardware/npu/cambricon.py
@@ -5,10 +5,12 @@
 @description: 寒武纪NPU信息采集
 """
 
+import math
 import platform
 import subprocess
 
-from ..type import HardwareFuncResult
+from ..type import HardwareFuncResult, HardwareInfoList, HardwareConfig, HardwareCollector as H
+from ..utils import generate_key, random_index
 
 
 def get_cambrian_npu_info() -> HardwareFuncResult:
@@ -20,6 +22,7 @@ def get_cambrian_npu_info() -> HardwareFuncResult:
         return None, None
 
     info = {"driver": None, "npu": None}
+    collector = None
     try:
         npu_map = map_npu()
         info["driver"] = npu_map[list(npu_map.keys())[0]]["driver"]
@@ -27,10 +30,11 @@ def get_cambrian_npu_info() -> HardwareFuncResult:
         for npu_id in npu_map:
             npu_map[npu_id].pop("driver")
         info["npu"] = npu_map
+        collector = CambrianCollector(npu_map)
     except Exception:  # noqa
         if all(v is None for v in info.values()):
             return None, None
-    return info, None
+    return info, collector
 
 
 def map_npu() -> dict:
@@ -63,3 +67,119 @@ def map_npu() -> dict:
             continue
 
     return npu_map
+
+
+class CambrianCollector(H):
+    def __init__(self, npu_map):
+        super().__init__()
+        self.npu_map = npu_map
+        # NPU Utilization (%)
+        self.util_key = generate_key("npu.{npu_index}.ptc")
+        util_config = HardwareConfig(
+            y_range=(0, 100),
+            chart_index=random_index(),
+            chart_name="NPU Utilization (%)",
+        )
+        self.per_util_configs = {}
+        # NPU Memory Allocated (%)
+        self.hbm_rate_key = generate_key("npu.{npu_index}.mem.ptc")
+        hbm_rate_config = HardwareConfig(
+            y_range=(0, 100),
+            chart_index=random_index(),
+            chart_name="NPU Memory Allocated (%)",
+        )
+        self.per_hbm_configs = {}
+        self.per_temp_configs = {}
+
+        for npu_id in npu_map:
+            metric_name = f"NPU {npu_id}"
+            self.per_util_configs[metric_name] = util_config.clone(metric_name=metric_name)
+            self.per_hbm_configs[metric_name] = hbm_rate_config.clone(metric_name=metric_name)
+
+    def collect(self) -> HardwareInfoList:
+        result: HardwareInfoList = []
+        for key, value in self.get_usage().items():
+            result.append(value)
+        for key, value in self.get_hbm_usage().items():
+            result.append(value)
+        return result
+
+    def get_usage(self) -> HardwareInfoList:
+        """
+        获取指定NPU设备的利用率
+        """
+        output = subprocess.run(
+            ["cnmon", "info", "-u"],
+            capture_output=True,
+            text=True,
+        ).stdout
+        # 获取信息
+        
+        util_infos = {}
+        npu_ids = []
+        
+        # 获取所有NPU ID
+        for npu_id in self.npu_map:
+            npu_ids.append(npu_id)
+        
+        index = 0
+        lines = output.split("\n")
+        for line in lines:
+            if "mlu average" in line.lower():             
+                util_infos[npu_ids[index]] = {
+                    "key": self.util_key.format(npu_index=npu_ids[index]),
+                    "name": f"NPU {npu_ids[index]} Utilization (%)",
+                    "value": math.nan,
+                    "config": self.per_util_configs[f"NPU {npu_ids[index]}"],
+                }
+                line = line.split(":")
+                # 利用率的值在最后一个
+                util = line[-1].replace("%", "").strip()
+                if util.isdigit():
+                    util_infos[npu_ids[index]]['value'] = float(util)
+                index += 1
+                continue
+                
+        return util_infos
+    
+    def get_hbm_usage(self) -> HardwareInfoList:
+        """
+        获取指定NPU设备的HBM利用率
+        """
+        output = subprocess.run(
+            ["cnmon", "info", "-m"],
+            capture_output=True,
+            text=True,
+        ).stdout
+        # 获取信息
+        
+        util_infos = {}
+        npu_ids = []
+        
+        # 获取所有NPU ID
+        for npu_id in self.npu_map:
+            npu_ids.append(npu_id)
+    
+        index = 0
+        lines = output.split("\n")
+        for line_index, line in enumerate(lines):
+            # 如果包含mlu average，则表示该行是NPU的利用率
+            if "physical memory usage" in line.lower():
+                if "used" in lines[line_index + 2].lower():
+                    used_line = lines[line_index + 2]
+                    # 初始化NPU的利用率
+                    util_infos[npu_ids[index]] = {
+                        "key": self.hbm_rate_key.format(npu_index=npu_ids[index]),
+                        "name": f"NPU {npu_ids[index]} Memory Allocated (%)",
+                        "value": math.nan,
+                        "config": self.per_hbm_configs[f"NPU {npu_ids[index]}"],
+                    }
+                    used_line = used_line.split(":")
+                    # 利用率的值在最后一个
+                    util = used_line[-1].replace("MiB", "").strip()
+                    if util.isdigit():
+                        util_infos[npu_ids[index]]['value'] = float(util)
+                    index += 1
+                    continue
+                
+        return util_infos
