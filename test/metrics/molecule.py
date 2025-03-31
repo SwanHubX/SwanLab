5bc9230285d54ad287ffab5bbfd6a0a7738faf9f
xj63 <xj63work@163.com>
2025-03-31 18:34:43 +0800

    chore: change test example molecule file path.

--- test/metrics/molecule.py
+++ test/metrics/molecule.py
@@ -10,9 +10,12 @@ molecule = swanlab.Object3D(chem, caption="cco")
 swanlab.log({"example": molecule})
 
 # from file path
-mol2 = swanlab.Object3D("./molecule.example.pdb")
+mol2 = swanlab.Object3D("./assets/molecule.example.pdb")
 swanlab.log({"file": mol2})
 
 # this file is from https://github.com/SwanHubX/SwanLab/pull/477
-mol3 = swanlab.Object3D("./big_mol.example.pdb")
-swanlab.log({"big_mol": mol3})
+# You should download https://github.com/SwanHubX/SwanLab/files/15022006/5p21.pdb.zip
+# and unzip it and rename to big_mol.example.pdb
+#
+# mol3 = swanlab.Object3D("./big_mol.example.pdb")
+# swanlab.log({"big_mol": mol3})
