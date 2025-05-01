521b395e7a01ff2c6e6da44b807da7f52b9a6fcc
Kang Li <79990647+SAKURA-CAT@users.noreply.github.com>
2025-05-01 21:21:15 +0800

    chore: revert upload column (#959)

--- swanlab/swanlab_settings.py
+++ swanlab/swanlab_settings.py
@@ -49,7 +49,7 @@ class Settings(BaseModel):
     )
     # ---------------------------------- 日志上传部分 ----------------------------------
     # 日志上传间隔
-    upload_interval: PositiveInt = 3
+    upload_interval: PositiveInt = 1
     # 终端日志上传单行最大字符数
     max_log_length: int = Field(ge=500, le=4096, default=1024)
 
