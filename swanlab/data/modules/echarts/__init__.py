b8f793ee228c6439e96a27c65f49d245ac00e524
ComPleHN <ghn108@163.com>
2025-05-19 18:59:13 +0800

    Update __init__.py
    一次导出

--- swanlab/data/modules/echarts/__init__.py
+++ swanlab/data/modules/echarts/__init__.py
@@ -5,13 +5,17 @@ class Echarts(MediaType):
     def __init__(self, chart: Base):
         self._chart = chart
         self.buffer = MediaBuffer()
-        self.buffer.write(self._chart.dump_options().encode('utf-8'))  # 写入二进制数据
-        self.buffer.seek(0)  # 重置指针到开头，以便后续读取
      # ---------------------------------- 覆写方法 ----------------------------------
 
     def parse(self):
         # 文件名称
-        hash_name = D.get_hash_by_bytes(self._chart.dump_options().encode('utf-8'))[:16]
+        byte_string = self._chart.dump_options().encode('utf-8')
+        hash_name = D.get_hash_by_bytes(byte_string)[:16]
+
+        # 写入buffer
+        self.buffer.write(byte_string)  # 写入二进制数据
+        self.buffer.seek(0)  # 重置指针到开头，以便后续读取
+
         filename = f"echart-step{self.step}-{hash_name}.json"
         return filename,self.buffer
 
