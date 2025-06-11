89509d35c0c3f90f8451a01fe027c1e12796d748
kites262 <47177568+kites262@users.noreply.github.com>
2025-06-11 14:19:59 +0800

    fix(test): openapi arg (#1075)

--- test/unit/api/openapi/test_experiment.py
+++ test/unit/api/openapi/test_experiment.py
@@ -12,19 +12,7 @@ import pytest
 
 import tutils as T
 from swanlab import OpenApi
-from swanlab.api.openapi.types import ApiResponse, Experiment, Pagination
-
-
-@pytest.mark.skipif(T.is_skip_cloud_test, reason="skip cloud test")
-def test_get_exp_state():
-    """
-    获取一个实验的状态
-    """
-    api = OpenApi()
-    res = api.get_exp_state(project="test_project", exp_cuid="test_cuid")
-    assert isinstance(res, ApiResponse)
-    if res.code == 200:
-        assert res.state == "FINISHED" or res.state == "RUNNING"
+from swanlab.api.openapi.types import ApiResponse, Experiment
 
 
 @pytest.mark.skipif(T.is_skip_cloud_test, reason="skip cloud test")
@@ -34,18 +22,18 @@ def test_get_experiment():
     """
     api = OpenApi()
     exp_cuid = "test_cuid"
-    res = api.get_experiment(project="test_project", exp_cuid=exp_cuid)
+    res = api.get_experiment(project="test_project", exp_id=exp_cuid)
     assert isinstance(res, ApiResponse)
     if res.code == 200:
         assert isinstance(res.data, Experiment)
 
 @pytest.mark.skipif(T.is_skip_cloud_test, reason="skip cloud test")
-def test_list_project_exps():
+def test_list_experiments():
     """
     获取一个项目下的实验列表
     """
     api = OpenApi()
-    res = api.list_project_exps(project="SwanLab")
+    res = api.list_experiments(project="SwanLab")
     assert isinstance(res, ApiResponse)
     if res.code == 200:
         assert isinstance(res.data, list)
@@ -53,13 +41,13 @@ def test_list_project_exps():
             assert isinstance(item, Experiment)
 
 @pytest.mark.skipif(T.is_skip_cloud_test, reason="skip cloud test")
-def test_get_exp_summary():
+def test_get_summary():
     """
     获取一个实验的Summary信息
     """
     api = OpenApi()
     exp_cuid = "test_cuid"
-    res = api.get_exp_summary(project="test_project", exp_cuid=exp_cuid)
+    res = api.get_summary(project="test_project", exp_id=exp_cuid)
     assert isinstance(res, ApiResponse)
     if res.code == 200:
         assert isinstance(res.data, dict)
