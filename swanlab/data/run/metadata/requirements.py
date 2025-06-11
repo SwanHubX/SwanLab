146afc62881a8c6058606fe66efc050e675003b6
Ze-Yi LIN <58305964+Zeyi-Lin@users.noreply.github.com>
2025-06-11 13:37:11 +0800

    fix: pip list freeze (#1073)
    Co-authored-by: KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>

--- swanlab/data/run/metadata/requirements.py
+++ swanlab/data/run/metadata/requirements.py
@@ -23,9 +23,11 @@ def get_requirements():
     except FileNotFoundError:
         pass
 
-    # 运行pip命令获取当前环境下的环境目录
-    result = subprocess.run(["pip", "list", "--format=freeze"], capture_output=True, text=True, timeout=15)
-    if result.returncode == 0:
-        return result.stdout
+    try:
+        result = subprocess.run(["pip", "list", "--format=freeze"], capture_output=True, text=True, timeout=15)
+        if result.returncode == 0:
+            return result.stdout
+    except Exception:  # noqa: 捕获所有异常，避免因环境问题导致程序崩溃
+        pass
 
     return None
