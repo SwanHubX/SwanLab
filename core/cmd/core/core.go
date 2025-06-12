1597f4e12abceec2a6dc3d749b7615d3d7ed3220
KAAANG <79990647+SAKURA-CAT@users.noreply.github.com>
2025-06-12 11:42:48 +0800

    chore: change module name

--- core/cmd/core/core.go
+++ core/cmd/core/core.go
@@ -12,7 +12,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
-	grpc_health "google.golang.org/grpc/health/grpc_health_v1"
+	grpcHealth "google.golang.org/grpc/health/grpc_health_v1"
 
 	"github.com/SwanHubX/SwanLab/core/internal/service"
 	"github.com/SwanHubX/SwanLab/core/pkg/pb"
@@ -30,11 +30,11 @@ func main() {
 	s := grpc.NewServer()
 	// Health Check Service
 	healthCheck := health.NewServer()
-	grpc_health.RegisterHealthServer(s, healthCheck)
+	grpcHealth.RegisterHealthServer(s, healthCheck)
 	// Collector Service
 	collector := &service.Collector{}
 	pb.RegisterCollectorServer(s, collector)
-	healthCheck.SetServingStatus("collector", grpc_health.HealthCheckResponse_SERVING)
+	healthCheck.SetServingStatus("collector", grpcHealth.HealthCheckResponse_SERVING)
 	slog.Info("server listening at", "address", lis.Addr())
 
 	if err = s.Serve(lis); err != nil {
