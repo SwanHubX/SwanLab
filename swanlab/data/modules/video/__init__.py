c5e3f8c38aa9191b8b334347b5cb0892d09c2282
ZeYi Lin <944270057@qq.com>
2025-07-16 23:12:41 +0800

    fix

--- swanlab/data/modules/video/__init__.py
+++ swanlab/data/modules/video/__init__.py
@@ -1,8 +1,13 @@
 from swanlab.data.modules.image import Image
+from typing import Union
+
 class Video(Image):    
     def __init__(
         self,
         data_or_path: str,
         caption: str = None,
+        mode: str = None,
+        file_type: str = None,
+        size: Union[int, list, tuple] = None,
     ):
-        super().__init__(data_or_path, caption=caption)
+        super().__init__(data_or_path, caption=caption, mode=mode, file_type=file_type, size=size)
