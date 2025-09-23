1afb266be904f6d7f7b60705084a915f7ffb2b2b
Kang Li <cunykang@gmail.com>
2025-09-23 11:42:26 +0800

    Revert "Update experiment.py"
    This reverts commit b569f82ef7010d74233dbbba65ad038f72defb84.

--- swanlab/api/experiment.py
+++ swanlab/api/experiment.py
@@ -11,7 +11,6 @@ from typing import List
 
 from swanlab.api.base import ApiBase, ApiHTTP
 from swanlab.api.types import ApiResponse, Experiment, Pagination
-from swanlab.package import get_host_api
 
 try:
     from pandas import DataFrame
@@ -180,12 +179,7 @@ class ExperimentAPI(ApiBase):
             if resp.errmsg:
                 continue
 
-            url:str = resp.data.get("url", "")
-            # 私有化环境可能不会携带 ip：https://github.com/SwanHubX/SwanLab/issues/1267
-            if not (url.startswith('https://') or url.startswith('http://')):
-                url = get_host_api() + url  # url 已添加前缀 /
-
-
+            url = resp.data.get("url", "")
             df = pd.read_csv(url, index_col=0)
 
             if idx == 0:
