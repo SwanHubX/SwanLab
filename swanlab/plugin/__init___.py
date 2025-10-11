0a7a584e9ffcd1de89be830724262cb60530d7e4
Kang Li <cunykang@gmail.com>
2025-10-11 01:48:37 -0400

    Update __init__.py

--- swanlab/plugin/__init___.py
+++
@@ -1,4 +0,0 @@
-from .notification import EmailCallback, LarkCallback, PrintCallback, SlackCallback, DingTalkCallback, WXWorkCallback, DiscordCallback
-from .writer import CSVWriter, LogdirFileWriter
-
-__all__ = ["EmailCallback", "LarkCallback", "PrintCallback", "CSVWriter", "WebhookCallback", "DingTalkCallback", "WXWorkCallback", "DiscordCallback", "SlackCallback", "LogdirFileWriter"]
