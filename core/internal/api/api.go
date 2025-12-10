b4d05df0b752197f36e94f21255a12e62e7387af
Kang Li <cunykang@gmail.com>
2025-12-10 22:28:08 +0800

    Update comments and dictionary entries for clarity
    Added 'cunyue' to the project dictionary and updated inline comments in Go files to use proper comment syntax for clarity and consistency.

--- core/internal/api/api.go
+++ core/internal/api/api.go
@@ -2,4 +2,4 @@
 // @Description  SwanLab API intended for requests to the SwanLab Server.
 // @Create       cunyue 2025/6/10 13:00
 
-package api //nolint:revive 暂时不改名
+package api //nolint:revive // 暂时不改名
