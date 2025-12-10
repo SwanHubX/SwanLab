83c12be74ea92787e38270fec069824f8ed59a45
Kang Li <cunykang@gmail.com>
2025-12-10 22:31:45 +0800

    Remove unused api.go and update parse.go package comment
    Deleted the core/internal/api/api.go file as it is no longer needed. Also updated the package comment in parse.go to remove the nolint directive.

--- core/internal/api/api.go
+++
@@ -1,5 +0,0 @@
-// @Title        api.go
-// @Description  SwanLab API intended for requests to the SwanLab Server.
-// @Create       cunyue 2025/6/10 13:00
-
-package api
--- core/internal/api/parse.go
+++ core/internal/api/parse.go
@@ -2,7 +2,7 @@
 // @Description  parse the proto message to json
 // @Create       cunyue 2025/6/12 18:49
 
-package api //nolint:revive // 暂时不改名
+package api
 
 import (
 	"errors"
