0fbc9976a907f199a6d3eb43f922a796f40c735b
Kang Li <cunykang@gmail.com>
2026-01-08 22:21:31 +0800

    Fix circular import by updating Api export
    Combined the import of OpenApi and Api from .api to prevent circular import issues and simplify the export process.

--- swanlab/__init__.py
+++ swanlab/__init__.py
@@ -23,8 +23,7 @@ SwanLabEnv.set_default()
 SwanLabEnv.check()
 
 # 导出 OpenApi 接口，必须要等待上述的 import 语句执行完毕以后才能导出，否则会触发循环引用
-from .api import OpenApi
-from . import Api
+from .api import OpenApi, Api
 
 __version__ = get_package_version()
 
