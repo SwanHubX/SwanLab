d277a96cb42f26d547a5c0e4709dd8f178b7b480
Bainianzzz <3036349123@qq.com>
2026-01-14 16:18:26 +0800

    able to modify session retry_times (disable the adapter when retries=0)
    - Temporarily modify the retry count in the adapter by injecting the request count into the request headers.

--- swanlab/core_python/client/session.py
+++ swanlab/core_python/client/session.py
@@ -5,12 +5,15 @@
 @description: 创建会话
 """
 
+import copy
+
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
 from swanlab.package import get_package_version
 
+RETRY_HEADER = "X-Custom-Retry"
 # 设置默认超时时间为 60s
 DEFAULT_TIMEOUT = 60
 
@@ -24,18 +27,41 @@ class TimeoutHTTPAdapter(HTTPAdapter):
 
     def send(self, request, **kwargs):
         # 如果 kwargs 中没有显式设置 timeout，则使用 self.timeout
-        if "timeout" not in kwargs and self.timeout is not None:
-            kwargs["timeout"] = self.timeout
+        if "timeout" not in kwargs:
+            kwargs["timeout"] = self.timeout if self.timeout is not None else DEFAULT_TIMEOUT
+
+        # 检查 headers 中是否有用户注入的重试次数
+        _retry = request.headers.pop(RETRY_HEADER, None)
+        if _retry is not None:
+            _adapter = copy.copy(self)
+            _adapter.max_retries = self.max_retries.new(total=int(_retry))
+
+            return _adapter.send(request, **kwargs)
 
         return super().send(request, **kwargs)
 
 
-def create_session() -> requests.Session:
+class SwanSession(requests.Session):
+    def request(self, method, url, *args, **kwargs):
+        retries = kwargs.pop('retries', None)
+
+        # 将用户指定的重试次数注入到 headers 中
+        if retries is not None:
+            headers = kwargs.get('headers')
+            if headers is None:
+                kwargs['headers'] = {RETRY_HEADER: str(retries)}
+            else:
+                headers[RETRY_HEADER] = str(retries)
+
+        return super().request(method, url, *args, **kwargs)
+
+
+def create_session() -> SwanSession:
     """
     创建一个带重试机制的会话
     :return: requests.Session
     """
-    session = requests.Session()
+    session = SwanSession()
     retry = Retry(
         total=5,
         backoff_factor=0.5,
