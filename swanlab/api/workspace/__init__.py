83726a1bd4ac5c912dfa6c7b7cf6687c7663948b
Bainianzzz <3036349123@qq.com>
2026-01-27 21:42:36 +0800

    feat: get workspaces through OpenApi
    - add new api get_workspace_info()
    - add new class Workspaces and Workspace

---
+++ swanlab/api/workspace/__init__.py
@@ -0,0 +1,72 @@
+"""
+@author: Zhou Qiyang
+@file: __init__.py
+@time: 2026/1/27 20:43
+@description: 工作空间
+"""
+
+from typing import Literal, Dict
+
+from swanlab.api.utils import get_properties
+from swanlab.core_python import Client
+from swanlab.core_python.api.type import WorkspaceType, RoleType
+from swanlab.core_python.api.user import get_workspace_info
+
+
+class Workspace:
+    def __init__(self, *, data: WorkspaceType = None, client: Client = None, workspace: str = None) -> None:
+        self._client = client
+
+        if data is None:
+            if workspace is None or client is None:
+                raise ValueError('username or client cannot both None')
+            data = get_workspace_info(self._client, workspace=workspace)
+        self._data = data
+
+    @property
+    def name(self) -> str:
+        """
+        Workspace display name.
+        """
+        return self._data['name']
+
+    @property
+    def username(self) -> str:
+        """
+        Workspace name.
+        """
+        return self._data['username']
+
+    @property
+    def workspace_type(self) -> Literal['TEAM', 'PERSON']:
+        """
+        Workspace type.
+        """
+        return self._data['type']
+
+    @property
+    def profile(self) -> Dict[str, str]:
+        """
+        Workspace profile.
+        """
+        return self._data['profile']
+
+    @property
+    def comment(self) -> str:
+        """
+        Workspace comment.
+        """
+        return self._data['comment']
+
+    @property
+    def role(self) -> RoleType:
+        """
+        Current login user's role in the workspace (only display when type=TEAM).
+        """
+        return self._data['role']
+
+    def json(self):
+        """
+        JSON-serializable dict of all @property values.
+        """
+        return get_properties(self)
--- swanlab/core_python/api/user/__init__.py
+++ swanlab/core_python/api/user/__init__.py
@@ -7,7 +7,7 @@
 
 from typing import TYPE_CHECKING, List
 
-from swanlab.core_python.api.type import GroupType, ApiKeyType
+from swanlab.core_python.api.type import GroupType, ApiKeyType, WorkspaceType
 from .self_hosted import get_self_hosted_init, create_user
 
 if TYPE_CHECKING:
@@ -34,11 +34,21 @@ def delete_api_key(client: "Client", *, key_id: int) -> None:
 
 def get_user_groups(client: "Client", *, username: str) -> List[GroupType]:
     """
-    获取当前全部的api_key
+    获取用户加入的组织
     :param client: 已登录的客户端实例
     :param username: 用户名称
     """
     res = client.get(f"/user/{username}/groups")
+    return res[0]
+
+
+def get_workspace_info(client: "Client", *, workspace: str) -> WorkspaceType:
+    """
+    获取指定工作空间的信息
+    :param client: 已登录的客户端实例
+    :param workspace: 工作空间名称
+    """
+    res = client.get(f"/group/{workspace}")
     return res[0]
 
 
@@ -64,6 +74,7 @@ __all__ = [
     "create_api_key",
     "delete_api_key",
     "get_user_groups",
+    "get_workspace_info",
     "get_api_keys",
     "get_latest_api_key",
     "get_self_hosted_init",
