04278cdfbff4aa3c943050c1588e8b54f28c9e3a
Nexisato <978452096@qq.com>
2026-04-21 15:36:26 +0800

    chore: update type def

--- swanlab/sdk/typings/core_python/api/project.py
+++ swanlab/sdk/typings/core_python/api/project.py
@@ -5,7 +5,7 @@
 @description: SwanLab 运行时项目API类型
 """
 
-from typing import Dict, List, Literal, TypedDict
+from typing import Dict, List, TypedDict
 
 from swanlab.sdk.typings.run import VisibilityType
 
@@ -13,12 +13,22 @@ from .common import LabelType
 
 
 class _ProjectCount(TypedDict):
+    # 项目历史实验数量
+    experiments: int
+    # 项目贡献者
+    contributors: int
+    # 项目协作者
+    collaborators: int
+    # 项目被clone次数
     clones: int
 
 
 class ProjectType(TypedDict):
     # 项目名称
     name: str
+    # 项目所属的用户名
+    username: str
+    # 项目路径 '/:username/:name'
     path: str
     # 项目可见性
     visibility: VisibilityType
@@ -27,13 +37,9 @@ class ProjectType(TypedDict):
     # 项目所属工作空间
     group: Dict[str, str]
     # 项目标签
-    projectLabels: LabelType
+    projectLabels: List[LabelType]
     # 项目统计信息
     _count: _ProjectCount
-
-    username: str
-    # 项目路径 '/:username/:name'
-    path: str
 
 
 class InitProjectType(TypedDict):
