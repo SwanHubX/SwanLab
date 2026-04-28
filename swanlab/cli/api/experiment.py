cab45f0a678598bf6e27ab7efc5d559bd391640e
neyuki778 <2597605722@qq.com>
2026-04-28 13:15:48 +0800

    feat: add list experiments

--- swanlab/cli/api/experiment.py
+++ swanlab/cli/api/experiment.py
@@ -1,10 +1,11 @@
 import click
 import orjson
 
+from swanlab.api.typings.common import ApiResponseType
 from swanlab.cli.api.helper import format_output, save_output, with_custom_host
 
 
-@click.group("run")
+@click.group("experiment")
 def experiment_cli():
     """Experiment(Run) management commands."""
     pass
@@ -25,6 +26,49 @@ def experiment_cli():
 def get_experiment(path: str, name, api):
     """Get Experiment(Run) info by path (username/project/run_id)."""
     resp = api.run(path).wrapper()
+    format_output(resp)
+    if resp.ok and name is not None:
+        save_output(orjson.dumps(resp.json(), option=orjson.OPT_INDENT_2), name=name)
+
+
+@experiment_cli.command("list")
+@click.option(
+    "--page_num",
+    "--page-num",
+    "-n",
+    default=1,
+    type=int,
+    help="Page number.",
+)
+@click.option(
+    "--page_size",
+    "--page-size",
+    "-s",
+    default=20,
+    type=int,
+    help="Page size.",
+)
+@click.option(
+    "--project_path",
+    "--project-path",
+    "-p",
+    required=True,
+    type=str,
+    help="Project path, like username/project_name.",
+)
+@click.option("--all", "fetch_all", is_flag=True, default=False, help="Fetch all pages.")
+@click.option(
+    "--save",
+    "name",
+    is_flag=False,
+    flag_value=".",
+    default=None,
+    help="Save output as JSON to current directory.",
+)
+@with_custom_host
+def list_experiments(page_num: int, page_size: int, project_path: str, fetch_all: bool, name, api):
+    """List experiments under a project."""
+    resp = ApiResponseType(ok=True, data=api.runs_get(path=project_path, page=page_num, size=page_size, all=fetch_all))
     format_output(resp)
     if resp.ok and name is not None:
         save_output(orjson.dumps(resp.json(), option=orjson.OPT_INDENT_2), name=name)
