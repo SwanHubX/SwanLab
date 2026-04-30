36250cbac7f15bbdaa1514df6631ecd9a4aa5f81
Kang Li <cunykang@gmail.com>
2026-04-30 14:11:28 +0800

    Update __init__.py

--- swanlab/__init__.py
+++ swanlab/__init__.py
@@ -100,13 +100,19 @@ def __getattr__(name: str):
 
 @deprecated("use `swanlab.echarts.roc_curve()` instead")
 def roc_curve(*args, **kwargs):
-    warnings.warn("`swanlab.roc_curve()` is deprecated, use `swanlab.echarts.roc_curve()` instead", DeprecationWarning)
+    warnings.warn(
+        "`swanlab.roc_curve()` is deprecated, use `swanlab.echarts.roc_curve()` instead",
+        DeprecationWarning,
+        stacklevel=2,
+    )
     return echarts.roc_curve(*args, **kwargs)
 
 
 @deprecated("use `swanlab.echarts.pr_curve()` instead")
 def pr_curve(*args, **kwargs):
-    warnings.warn("`swanlab.pr_curve()` is deprecated, use `swanlab.echarts.pr_curve()` instead", DeprecationWarning)
+    warnings.warn(
+        "`swanlab.pr_curve()` is deprecated, use `swanlab.echarts.pr_curve()` instead", DeprecationWarning, stacklevel=2
+    )
     return echarts.pr_curve(*args, **kwargs)
 
 
@@ -115,5 +121,6 @@ def confusion_matrix(*args, **kwargs):
     warnings.warn(
         "`swanlab.confusion_matrix()` is deprecated, use `swanlab.echarts.confusion_matrix()` instead",
         DeprecationWarning,
+        stacklevel=2,
     )
     return echarts.confusion_matrix(*args, **kwargs)
