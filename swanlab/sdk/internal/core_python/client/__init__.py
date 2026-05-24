bb06498e05296afe411afa0a269a6cd2f906b439
Kang Li <cunykang@gmail.com>
2026-05-24 16:49:04 +0800

    Clarify client description against backend API

--- swanlab/sdk/internal/core_python/client/__init__.py
+++ swanlab/sdk/internal/core_python/client/__init__.py
@@ -2,7 +2,7 @@
 @author: cunyue
 @file: __init__.py
 @time: 2026/3/7 14:04
-@description: SwanLab 运行时客户端，用于与 SwanLab API 进行交互
+@description: SwanLab 运行时客户端，用于与 SwanLab 后端 API 进行交互
 """
 
 from typing import Optional
